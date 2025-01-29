66c68caf49a8b474d5ba671e826e523c0a6b0a6b
YBM <yan@avara.xyz>
2025-01-29 09:48:53 -0600

    chore: fix comments

--- tests/unit/LiquidityHub/LiquidityHubTest.Restore.t.sol
+++ tests/unit/LiquidityHub/LiquidityHubTest.Restore.t.sol
@@ -367,7 +367,7 @@ contract LiquidityHubRestoreTest is LiquidityHubBaseTest {
     uint256 wethAmount = 10e18;
 
     drawAmount = bound(drawAmount, 1, daiAmount); // within supplied dai amount
-    skipTime = bound(skipTime, 1, 365 * 10 * 1 days); // 1 day to 10 years
+    skipTime = bound(skipTime, 1, 365 * 10 * 1 days); // 1 sec to 10 years
     rate = bound(rate, 0, 200_00).bpsToRay(); // .1% to 200%
     riskPremiumRad = bound(riskPremiumRad, 0, maxRiskPremiumRad);
 
@@ -524,7 +524,7 @@ contract LiquidityHubRestoreTest is LiquidityHubBaseTest {
     uint256 wethAmount = 10e18;
 
     drawAmount = bound(drawAmount, 1, daiAmount); // within supplied dai amount
-    skipTime = bound(skipTime, 1, 365 * 10 * 1 days); // 1 day to 10 years
+    skipTime = bound(skipTime, 1, 365 * 10 * 1 days); // 1 sec to 10 years
     rate = bound(rate, 0, 200_00).bpsToRay(); // 0% to 200%
     riskPremiumRad = bound(riskPremiumRad, 1, maxRiskPremiumRad);
 
@@ -664,7 +664,7 @@ contract LiquidityHubRestoreTest is LiquidityHubBaseTest {
     uint256 wethAmount = 10e18;
 
     drawAmount = bound(drawAmount, 1, daiAmount); // within supplied dai amount
-    skipTime = bound(skipTime, 1, 365 * 10 * 1 days); // 1 day to 10 years
+    skipTime = bound(skipTime, 1, 365 * 10 * 1 days); // 1 sec to 10 years
     rate = bound(rate, 0, 200_00).bpsToRay(); // 0% to 200%
     riskPremiumRad = bound(riskPremiumRad, 0, maxRiskPremiumRad);
 
