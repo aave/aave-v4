7f657c560043b8c585018c0286343aed90a53613
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-02-06 19:26:44 -0800

    test: Borrow fuzz amounts

--- tests/unit/Spoke/SpokeTest.t.sol
+++ tests/unit/Spoke/SpokeTest.t.sol
@@ -203,6 +203,89 @@ contract SpokeTest is BaseTest {
     assertEq(tokenList.weth.balanceOf(address(spoke2)), 0, 'spoke2 weth final balance');
   }
 
+  function test_borrow_revertsWith_not_available_liquidity() public {
+    uint256 daiAmount = 100e18;
+    uint256 wethAmount = 10e18;
+
+    // Bob supply weth
+    deal(address(tokenList.weth), bob, wethAmount);
+    Utils.spokeSupply(hub, spoke1, wethAssetId, bob, wethAmount, bob);
+
+    // Alice supply dai
+    deal(address(tokenList.dai), alice, daiAmount);
+    Utils.spokeSupply(hub, spoke1, daiAssetId, alice, daiAmount, alice);
+
+    // Bob draw more than supplied dai amount
+    vm.prank(bob);
+    vm.expectRevert('NOT_AVAILABLE_LIQUIDITY');
+    spoke1.borrow(daiAssetId, bob, daiAmount + 1);
+  }
+
+  function test_borrow_fuzz_amounts(uint256 wethSupplyAmount, uint256 daiBorrowAmount) public {
+    wethSupplyAmount = bound(wethSupplyAmount, 1, MAX_SUPPLY_AMOUNT);
+    daiBorrowAmount = bound(daiBorrowAmount, 1, wethSupplyAmount / 2 + 1);
+
+    // Reset account balances
+    deal(address(tokenList.dai), bob, 0);
+    deal(address(tokenList.weth), alice, 0);
+
+    // Bob supply weth
+    deal(address(tokenList.weth), bob, wethSupplyAmount);
+    Utils.spokeSupply(hub, spoke1, wethAssetId, bob, wethSupplyAmount, bob);
+
+    // Alice supply dai
+    deal(address(tokenList.dai), alice, daiBorrowAmount);
+    Utils.spokeSupply(hub, spoke1, daiAssetId, alice, daiBorrowAmount, alice);
+
+    Spoke.UserConfig memory bobData = spoke1.getUser(wethAssetId, bob);
+    Spoke.UserConfig memory aliceData = spoke1.getUser(daiAssetId, alice);
+
+    assertEq(
+      bobData.suppliedShares,
+      hub.convertToSharesDown(wethAssetId, wethSupplyAmount),
+      'bob supply shares pre-draw'
+    );
+    assertEq(bobData.baseDebt, 0, 'bob base debt pre-draw');
+    assertEq(
+      aliceData.suppliedShares,
+      hub.convertToSharesDown(daiAssetId, daiBorrowAmount),
+      'alice supply shares pre-draw'
+    );
+    assertEq(aliceData.baseDebt, 0, 'alice base debt pre-draw');
+    assertEq(tokenList.dai.balanceOf(address(spoke1)), 0, 'spoke1 dai balance pre-draw');
+    assertEq(tokenList.weth.balanceOf(address(spoke2)), 0, 'spoke2 weth balance pre-draw');
+    assertEq(tokenList.dai.balanceOf(bob), 0, 'bob dai balance pre-draw');
+    assertEq(tokenList.weth.balanceOf(alice), 0, 'alice weth balance pre-draw');
+
+    // Bob draw dai
+    vm.prank(bob);
+    vm.expectEmit(address(spoke1));
+    emit Borrowed(daiAssetId, bob, daiBorrowAmount);
+    spoke1.borrow(daiAssetId, bob, daiBorrowAmount);
+
+    bobData = spoke1.getUser(wethAssetId, bob);
+    aliceData = spoke1.getUser(daiAssetId, alice);
+
+    assertEq(
+      bobData.suppliedShares,
+      hub.convertToSharesDown(wethAssetId, wethSupplyAmount),
+      'bob supply shares final balance'
+    );
+    assertEq(bobData.baseDebt, 0, 'bob base debt weth final balance');
+    bobData = spoke1.getUser(daiAssetId, bob);
+    assertEq(bobData.baseDebt, daiBorrowAmount, 'bob base debt dai final balance');
+    assertEq(
+      aliceData.suppliedShares,
+      hub.convertToSharesDown(daiAssetId, daiBorrowAmount),
+      'alice supply shares final balance'
+    );
+    assertEq(aliceData.baseDebt, 0, 'alice base debt final');
+    assertEq(tokenList.dai.balanceOf(bob), daiBorrowAmount, 'bob dai final balance');
+    assertEq(tokenList.weth.balanceOf(alice), 0, 'alice weth final balance');
+    assertEq(tokenList.dai.balanceOf(address(spoke1)), 0, 'spoke1 dai final balance');
+    assertEq(tokenList.weth.balanceOf(address(spoke2)), 0, 'spoke2 weth final balance');
+  }
+
   function test_withdraw() public {
     // TODO: Add getter of asset id based on address
     uint256 amount = 100e18;
