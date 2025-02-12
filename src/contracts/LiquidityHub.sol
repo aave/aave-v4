fb8a9b69f2821211e54ba7a9e8c5bf335d928102
YBM <yan@avara.xyz>
2025-02-11 18:26:59 -0600

    feat: add comments on implications for init index

--- src/contracts/LiquidityHub.sol
+++ src/contracts/LiquidityHub.sol
@@ -113,6 +113,7 @@ contract LiquidityHub is ILiquidityHub {
     // TODO: emit event
   }
 
+  /// @dev MUST occur atomically with Spoke's addReserve to align baseBorrowIndex.
   function addSpoke(uint256 assetId, DataTypes.SpokeConfig memory config, address spoke) external {
     // TODO: AccessControl
     _addSpoke(assetId, config, spoke);
@@ -405,8 +406,8 @@ contract LiquidityHub is ILiquidityHub {
 
     uint256 newSpokeDebt = baseDebtChange > 0
       ? existingSpokeDebt + uint256(baseDebtChange) // debt added
-      : // force underflow: only possible when spoke takes repays amount more than net drawn
-      existingSpokeDebt - uint256(-baseDebtChange); // debt restored
+      // force underflow: only possible when spoke takes repays amount more than net drawn
+      : existingSpokeDebt - uint256(-baseDebtChange); // debt restored
 
     (uint256 newAssetRiskPremium, uint256 newAssetDebt) = MathUtils.addToWeightedAverage(
       assetRiskPremiumWithoutCurrent,
