e5e15b0d92a438e868756970d807f5c5487aee23
YBM <yan@avara.xyz>
2025-02-21 16:59:32 -0600

    test: fuzz with index increases

--- tests/unit/Spoke/Spoke.Supply.t.sol
+++ tests/unit/Spoke/Spoke.Supply.t.sol
@@ -22,6 +22,17 @@ contract SpokeSupplyTest is BaseTest {
     uint256 userBalance;
   }
 
+  struct CollateralReserve {
+    uint256 reserveId;
+    uint256 amount;
+  }
+
+  struct BorrowReserve {
+    uint256 reserveId;
+    uint256 amount;
+    address supplier;
+  }
+
   function setUp() public override {
     super.setUp();
     initEnvironment();
@@ -272,6 +283,83 @@ contract SpokeSupplyTest is BaseTest {
     );
   }
 
+  function test_supply_fuzz_index_increase_no_premium(uint256 amount) public {
+    amount = bound(amount, 1, MAX_SUPPLY_AMOUNT);
+
+    _increaseShareConversionIndex({
+      collateral: CollateralReserve({reserveId: spokeInfo[spoke1].weth.reserveId, amount: 100e18}),
+      borrow: BorrowReserve({
+        reserveId: spokeInfo[spoke1].dai.reserveId,
+        amount: 10e18,
+        supplier: bob
+      }),
+      borrower: alice,
+      rate: uint256(10_00).bpsToRay()
+    });
+
+    TestData[2] memory userData;
+    TestData[2] memory reserveData;
+    TokenData[2] memory tokenData;
+    uint256 phase = 0;
+
+    userData[phase] = _getUserData(spokeInfo[spoke1].dai.reserveId, carol);
+    reserveData[phase] = _getReserveData(spokeInfo[spoke1].dai.reserveId);
+    tokenData[phase] = _getTokenData(address(tokenList.dai), carol);
+
+    vm.assume(hub.convertToShares(daiAssetId, amount) > 0);
+
+    deal(address(tokenList.dai), carol, amount);
+
+    vm.prank(carol);
+    vm.expectEmit(address(spoke1));
+    emit Supplied(spokeInfo[spoke1].dai.reserveId, amount, carol);
+    spoke1.supply(spokeInfo[spoke1].dai.reserveId, amount);
+    phase = 1;
+
+    userData[phase] = _getUserData(spokeInfo[spoke1].dai.reserveId, carol);
+    reserveData[phase] = _getReserveData(spokeInfo[spoke1].dai.reserveId);
+    tokenData[phase] = _getTokenData(address(tokenList.dai), carol);
+
+    // dai balance
+    assertEq(tokenList.dai.balanceOf(carol), 0, 'user token balance post-supply');
+    assertEq(
+      tokenList.dai.balanceOf(address(hub)),
+      tokenData[phase - 1].hubBalance + amount,
+      'hub token balance post-supply'
+    );
+    assertEq(tokenList.dai.balanceOf(address(spoke1)), 0, 'spoke token balance post-supply');
+    // reserve
+    assertEq(
+      reserveData[phase].baseDebt,
+      reserveData[phase - 1].baseDebt,
+      'reserve baseDebt post-supply'
+    );
+    assertEq(reserveData[phase].outstandingPremium, 0, 'reserve outstandingPremium post-supply');
+    assertEq(
+      reserveData[phase].suppliedShares,
+      reserveData[phase - 1].suppliedShares + hub.convertToSharesDown(daiAssetId, amount),
+      'reserve suppliedShares post-supply'
+    );
+    assertEq(
+      reserveData[phase].lastUpdateTimestamp,
+      vm.getBlockTimestamp(),
+      'reserve lastUpdateTimestamp post-supply'
+    );
+    // user
+    assertEq(userData[phase].baseDebt, 0, 'user baseDebt post-supply');
+    assertEq(userData[phase].outstandingPremium, 0, 'user outstandingPremium post-supply');
+    assertEq(
+      userData[phase].suppliedShares,
+      hub.convertToSharesDown(daiAssetId, amount),
+      'user suppliedShares post-supply'
+    );
+    assertEq(
+      userData[phase].lastUpdateTimestamp,
+      vm.getBlockTimestamp(),
+      'user lastUpdateTimestamp post-supply'
+    );
+  }
+
   function test_supply_index_increase_with_premium() public {
     // alice supplies usdx as collateral, borrows dai
     _increaseShareConversionIndex({
@@ -361,35 +449,102 @@ contract SpokeSupplyTest is BaseTest {
     );
   }
 
-  // function test_supply_fuzz_index_increase_with_premium() public {
-  //   // fuzz supply/draw amount for Alice
-  //   // fuzz supply amount for bob
+  function test_supply_fuzz_index_increase_with_premium(uint256 amount) public {
+    amount = bound(amount, 1, MAX_SUPPLY_AMOUNT);
 
-  //   // Alice supply/draw to start index
-  //   // asset with LP > 0
-  //   // time skip to increase index
+    // alice supplies usdx as collateral, borrows dai
+    _increaseShareConversionIndex({
+      collateral: CollateralReserve({reserveId: spokeInfo[spoke1].usdx.reserveId, amount: 100e18}),
+      borrow: BorrowReserve({
+        reserveId: spokeInfo[spoke1].dai.reserveId,
+        amount: 10e18,
+        supplier: bob
+      }),
+      borrower: alice,
+      rate: uint256(10_00).bpsToRay()
+    });
 
-  //   vm.expectEmit(address(spoke1));
-  //   emit Supplied(spokeInfo[spoke1].dai.reserveId, amount, bob);
+    // action on the borrowed reserve to trigger risk premium
+    // TODO: shouldnt be needed after RP accrual is fixed
+    Utils.spokeSupply({
+      hub: hub,
+      spoke: spoke1,
+      reserveId: spokeInfo[spoke1].dai.reserveId,
+      user: alice,
+      amount: 1e18,
+      to: alice
+    });
+    skip(365 days);
 
-  //   vm.prank(bob);
-  //   spoke1.supply(spokeInfo[spoke1].dai.reserveId, amount);
-  // }
+    vm.assume(hub.convertToShares(daiAssetId, amount) > 0);
 
-  struct CollateralReserve {
-    uint256 reserveId;
-    uint256 amount;
+    TestData[2] memory userData;
+    TestData[2] memory reserveData;
+    TokenData[2] memory tokenData;
+    uint256 phase = 0;
+
+    userData[phase] = _getUserData(spokeInfo[spoke1].dai.reserveId, carol);
+    reserveData[phase] = _getReserveData(spokeInfo[spoke1].dai.reserveId);
+    tokenData[phase] = _getTokenData(address(tokenList.dai), carol);
+
+    assertTrue(reserveData[phase].outstandingPremium > 0, 'reserve outstandingPremium pre-supply');
+
+    uint256 amount = 1e18;
+    deal(address(tokenList.dai), carol, amount);
+
+    vm.prank(carol);
+    vm.expectEmit(address(spoke1));
+    emit Supplied(spokeInfo[spoke1].dai.reserveId, amount, carol);
+    spoke1.supply(spokeInfo[spoke1].dai.reserveId, amount);
+    phase = 1;
+
+    userData[phase] = _getUserData(spokeInfo[spoke1].dai.reserveId, carol);
+    reserveData[phase] = _getReserveData(spokeInfo[spoke1].dai.reserveId);
+    tokenData[phase] = _getTokenData(address(tokenList.dai), carol);
+
+    // dai balance
+    assertEq(tokenList.dai.balanceOf(carol), 0, 'user token balance post-supply');
+    assertEq(
+      tokenList.dai.balanceOf(address(hub)),
+      tokenData[phase - 1].hubBalance + amount,
+      'hub token balance post-supply'
+    );
+    assertEq(tokenList.dai.balanceOf(address(spoke1)), 0, 'spoke token balance post-supply');
+    // reserve
+    assertEq(
+      reserveData[phase].baseDebt,
+      reserveData[phase - 1].baseDebt,
+      'reserve baseDebt post-supply'
+    );
+    assertTrue(reserveData[phase].outstandingPremium > 0, 'reserve outstandingPremium post-supply');
+    assertEq(
+      reserveData[phase].suppliedShares,
+      reserveData[phase - 1].suppliedShares + hub.convertToSharesDown(daiAssetId, amount),
+      'reserve suppliedShares post-supply'
+    );
+    assertEq(
+      reserveData[phase].lastUpdateTimestamp,
+      vm.getBlockTimestamp(),
+      'reserve lastUpdateTimestamp post-supply'
+    );
+    // user
+    assertEq(userData[phase].baseDebt, 0, 'user baseDebt post-supply');
+    assertEq(userData[phase].outstandingPremium, 0, 'user outstandingPremium post-supply');
+    assertEq(
+      userData[phase].suppliedShares,
+      hub.convertToSharesDown(daiAssetId, amount),
+      'user suppliedShares post-supply'
+    );
+    assertEq(
+      userData[phase].lastUpdateTimestamp,
+      vm.getBlockTimestamp(),
+      'user lastUpdateTimestamp post-supply'
+    );
   }
 
-  struct BorrowReserve {
-    uint256 reserveId;
-    uint256 amount;
-    address supplier;
-  }
-
-  // increase share conversion index on reserveId
-  // supplies/draws reserve asset
-  // using weth as the collateral
+  // increase share conversion index on BorrowReserve asset
+  // using CollateralReserve asset the collateral
+  // supplies/draws BorrowReserve asset
   function _increaseShareConversionIndex(
     CollateralReserve memory collateral,
     BorrowReserve memory borrow,
@@ -443,7 +598,7 @@ contract SpokeSupplyTest is BaseTest {
     skip(365 days);
   }
 
-  function _getReserveData(uint256 reserveId) internal returns (TestData memory) {
+  function _getReserveData(uint256 reserveId) internal view returns (TestData memory) {
     // only to check lastUpdateTimestamp
     Spoke.Reserve memory reserveStorageData = spoke1.getReserve(reserveId);
     TestData memory reserveData;
@@ -453,7 +608,7 @@ contract SpokeSupplyTest is BaseTest {
     return reserveData;
   }
 
-  function _getUserData(uint256 reserveId, address user) internal returns (TestData memory) {
+  function _getUserData(uint256 reserveId, address user) internal view returns (TestData memory) {
     // only to check lastUpdateTimestamp
     Spoke.UserConfig memory userStorageData = spoke1.getUser(reserveId, user);
     TestData memory userData;
@@ -463,7 +618,7 @@ contract SpokeSupplyTest is BaseTest {
     return userData;
   }
 
-  function _getTokenData(address token, address user) internal returns (TokenData memory) {
+  function _getTokenData(address token, address user) internal view returns (TokenData memory) {
     TokenData memory tokenData;
     tokenData.spokeBalance = tokenList.dai.balanceOf(address(spoke1));
     tokenData.hubBalance = tokenList.dai.balanceOf(address(hub));
