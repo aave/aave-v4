a1a2ee0965b24c2f4422f966883d6b7c8d4662c5
YBM <yan@avara.xyz>
2025-02-21 16:22:30 -0600

    fix: return new newAggregatedRiskPremium

--- src/contracts/Spoke.sol
+++ src/contracts/Spoke.sol
@@ -508,8 +508,8 @@ contract Spoke is ISpoke {
 
     uint256 newUserDebt = baseDebtChange > 0
       ? existingUserDebt + uint256(baseDebtChange) // debt added
-      : // force underflow: only possible when user takes repays amount more than net drawn
-      existingUserDebt - uint256(-baseDebtChange); // debt restored
+      // force underflow: only possible when user takes repays amount more than net drawn
+      : existingUserDebt - uint256(-baseDebtChange); // debt restored
 
     (uint256 newReserveRiskPremium, uint256 newReserveDebt) = MathUtils.addToWeightedAverage(
       reserveRiskPremiumWithoutCurrent,
@@ -523,6 +523,8 @@ contract Spoke is ISpoke {
 
     reserve.riskPremium = newReserveRiskPremium;
     user.riskPremium = newUserRiskPremium;
+
+    return newReserveRiskPremium;
   }
 
   function _validateSetUsingAsCollateral(
