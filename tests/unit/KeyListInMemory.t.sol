bfd799dd5b96707a97d379256a2d6a6af470af78
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2025-03-05 17:18:56 +0530

    feat: change max limits in keyValueList

--- tests/unit/KeyListInMemory.t.sol
+++ tests/unit/KeyListInMemory.t.sol
@@ -14,13 +14,7 @@ contract KeyValueListInMemoryTest is Test {
       list.add(i, _truncateKey(seed[i]), _truncateValue(seed[i]));
     }
     list.sortByKey();
-    // validate sorted order
-    (uint256 prevKey, ) = list.get(0);
-    for (uint256 i = 1; i < seed.length; ++i) {
-      (uint256 key, ) = list.get(i);
-      assertLe(prevKey, key);
-      prevKey = key;
-    }
+    _assertSortedOrder(list);
   }
 
   function test_fuzz_sortByKey_length(uint256 length) public {
@@ -30,9 +24,26 @@ contract KeyValueListInMemoryTest is Test {
       list.add(i, _truncateKey(vm.randomUint()), _truncateValue(vm.randomUint()));
     }
     list.sortByKey();
+    _assertSortedOrder(list);
+  }
+
+  function test_fuzz_get(uint256[] memory seed) public pure {
+    vm.assume(seed.length > 0);
+    KeyValueListInMemory.List memory list = KeyValueListInMemory.init(seed.length);
+    for (uint256 i = 0; i < seed.length; ++i) {
+      list.add(i, _truncateKey(seed[i]), _truncateValue(seed[i]));
+    }
+    for (uint256 i; i < seed.length; ++i) {
+      (uint256 key, uint256 value) = list.get(i);
+      assertEq(key, _truncateKey(seed[i]));
+      assertEq(value, _truncateValue(seed[i]));
+    }
+  }
+
+  function _assertSortedOrder(KeyValueListInMemory.List memory list) internal pure {
     // validate sorted order
     (uint256 prevKey, ) = list.get(0);
-    for (uint256 i = 1; i < length; ++i) {
+    for (uint256 i = 1; i < list.length(); ++i) {
       (uint256 key, ) = list.get(i);
       assertLe(prevKey, key);
       prevKey = key;
@@ -40,9 +51,9 @@ contract KeyValueListInMemoryTest is Test {
   }
 
   function _truncateKey(uint256 key) internal pure returns (uint256) {
-    return key % (1 << KeyValueListInMemory._MAX_BIT_SIZE);
+    return key % KeyValueListInMemory._KEY_MASK;
   }
   function _truncateValue(uint256 value) internal pure returns (uint256) {
-    return value % (1 << KeyValueListInMemory._MAX_BIT_SIZE);
+    return value % KeyValueListInMemory._VALUE_MASK;
   }
 }
