0a94d32aaa49e6336cf4eb3666cf1da89637577f
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2025-03-25 02:47:34 +0530

    test: restore isolate, bring back unrelated tests

--- tests/unit/DefaultInterestRateStrategy.t.sol
+++ tests/unit/DefaultInterestRateStrategy.t.sol
@@ -52,180 +52,162 @@ contract DefaultReserveInterestRateStrategyTest is Test {
   }
 
   function test_SetReserveInterestRateParams() public {
-    vm.skip(true, 'pending refactor');
+    IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
 
-//     IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
+    vm.prank(mockAddressesProvider);
+    vm.expectEmit(true, false, false, true);
+    emit RateDataUpdate(
+      mockReserveAddress,
+      uint256(rateData.optimalUsageRatio),
+      uint256(rateData.baseVariableBorrowRate),
+      uint256(rateData.variableRateSlope1),
+      uint256(rateData.variableRateSlope2)
+    );
 
-//     vm.prank(mockAddressesProvider);
-//     vm.expectEmit(true, false, false, true);
-//     emit RateDataUpdate(
-//       mockReserveAddress,
-//       uint256(rateData.optimalUsageRatio),
-//       uint256(rateData.baseVariableBorrowRate),
-//       uint256(rateData.variableRateSlope1),
-//       uint256(rateData.variableRateSlope2)
-//     );
+    rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
 
-//     rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
-
-//     assertEq(address(rateStrategy.ADDRESSES_PROVIDER()), mockAddressesProvider);
-//     assertEq(rateStrategy.getOptimalUsageRatio(mockReserveAddress), rateData.optimalUsageRatio);
-//     assertEq(rateStrategy.getVariableRateSlope1(mockReserveAddress), rateData.variableRateSlope1);
-//     assertEq(rateStrategy.getVariableRateSlope2(mockReserveAddress), rateData.variableRateSlope2);
-//     assertEq(
-//       rateStrategy.getBaseVariableBorrowRate(mockReserveAddress),
-//       rateData.baseVariableBorrowRate
-//     );
-//     assertEq(
-//       rateStrategy.getMaxVariableBorrowRate(mockReserveAddress),
-//       rateData.baseVariableBorrowRate + rateData.variableRateSlope1 + rateData.variableRateSlope2
-//     );
-  
-}
+    assertEq(address(rateStrategy.ADDRESSES_PROVIDER()), mockAddressesProvider);
+    assertEq(rateStrategy.getOptimalUsageRatio(mockReserveAddress), rateData.optimalUsageRatio);
+    assertEq(rateStrategy.getVariableRateSlope1(mockReserveAddress), rateData.variableRateSlope1);
+    assertEq(rateStrategy.getVariableRateSlope2(mockReserveAddress), rateData.variableRateSlope2);
+    assertEq(
+      rateStrategy.getBaseVariableBorrowRate(mockReserveAddress),
+      rateData.baseVariableBorrowRate
+    );
+    assertEq(
+      rateStrategy.getMaxVariableBorrowRate(mockReserveAddress),
+      rateData.baseVariableBorrowRate + rateData.variableRateSlope1 + rateData.variableRateSlope2
+    );
+  }
 
   function test_calculate_interestRates_reserve_empty() public {
-    vm.skip(true, 'pending refactor');
+    IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
 
-//     IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
+    rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
 
-//     rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
+    uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
+      DataTypes.CalculateInterestRatesParams({
+        liquidityAdded: 0,
+        liquidityTaken: 0,
+        totalDebt: 0,
+        reserveFactor: 0,
+        assetId: mockReserveAddress,
+        virtualUnderlyingBalance: 0,
+        usingVirtualBalance: true
+      })
+    );
 
-//     uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
-//       DataTypes.CalculateInterestRatesParams({
-//         liquidityAdded: 0,
-//         liquidityTaken: 0,
-//         totalDebt: 0,
-//         reserveFactor: 0,
-//         assetId: mockReserveAddress,
-//         virtualUnderlyingBalance: 0,
-//         usingVirtualBalance: true
-//       })
-//     );
-
-//     assertEq(variableBorrowRate, 0);
-  
-}
+    assertEq(variableBorrowRate, 0);
+  }
 
   function test_calculate_interestRates_reserve_debt_80() public {
-    vm.skip(true, 'pending refactor');
+    IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
 
-//     IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
+    rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
 
-//     rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
+    uint256 availableLiquidity = 2e18;
+    uint256 totalDebt = 8e18;
 
-//     uint256 availableLiquidity = 2e18;
-//     uint256 totalDebt = 8e18;
+    uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
+      DataTypes.CalculateInterestRatesParams({
+        liquidityAdded: 0,
+        liquidityTaken: 0,
+        totalDebt: totalDebt,
+        reserveFactor: 0,
+        assetId: mockReserveAddress,
+        virtualUnderlyingBalance: availableLiquidity,
+        usingVirtualBalance: true
+      })
+    );
 
-//     uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
-//       DataTypes.CalculateInterestRatesParams({
-//         liquidityAdded: 0,
-//         liquidityTaken: 0,
-//         totalDebt: totalDebt,
-//         reserveFactor: 0,
-//         assetId: mockReserveAddress,
-//         virtualUnderlyingBalance: availableLiquidity,
-//         usingVirtualBalance: true
-//       })
-//     );
+    uint256 expectedVariableRate = rateStrategy.getBaseVariableBorrowRate(mockReserveAddress) +
+      rateStrategy.getVariableRateSlope1(mockReserveAddress);
 
-//     uint256 expectedVariableRate = rateStrategy.getBaseVariableBorrowRate(mockReserveAddress) +
-//       rateStrategy.getVariableRateSlope1(mockReserveAddress);
-
-//     assertEq(expectedVariableRate.bpsToRay(), variableBorrowRate, 'Invalid borrow rate');
-  
-}
+    assertEq(expectedVariableRate.bpsToRay(), variableBorrowRate, 'Invalid borrow rate');
+  }
 
   function test_calculate_interest_rate_100_usage_ratio() public {
-    vm.skip(true, 'pending refactor');
+    IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
+    rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
 
-//     IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
-//     rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
+    uint256 totalDebt = 1e18;
+    uint256 virtualUnderlyingBalance = 0;
 
-//     uint256 totalDebt = 1e18;
-//     uint256 virtualUnderlyingBalance = 0;
+    uint256 utilizationRatio = getUtilizationRatio(totalDebt, virtualUnderlyingBalance);
 
-//     uint256 utilizationRatio = getUtilizationRatio(totalDebt, virtualUnderlyingBalance);
+    uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
+      DataTypes.CalculateInterestRatesParams({
+        liquidityAdded: 0,
+        liquidityTaken: 0,
+        totalDebt: totalDebt,
+        reserveFactor: 0,
+        assetId: mockReserveAddress,
+        virtualUnderlyingBalance: virtualUnderlyingBalance,
+        usingVirtualBalance: true
+      })
+    );
 
-//     uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
-//       DataTypes.CalculateInterestRatesParams({
-//         liquidityAdded: 0,
-//         liquidityTaken: 0,
-//         totalDebt: totalDebt,
-//         reserveFactor: 0,
-//         assetId: mockReserveAddress,
-//         virtualUnderlyingBalance: virtualUnderlyingBalance,
-//         usingVirtualBalance: true
-//       })
-//     );
+    uint256 optimalUsageRatio = rateData.optimalUsageRatio;
+    uint256 excessBorrowUsageRatio = ((utilizationRatio - optimalUsageRatio) * 10000) /
+      optimalUsageRatio;
 
-//     uint256 optimalUsageRatio = rateData.optimalUsageRatio;
-//     uint256 excessBorrowUsageRatio = ((utilizationRatio - optimalUsageRatio) * 10000) /
-//       optimalUsageRatio;
+    uint256 expectedVariableRate = rateData.baseVariableBorrowRate +
+      ((rateData.variableRateSlope1 + rateData.variableRateSlope2) * excessBorrowUsageRatio) /
+      10000;
 
-//     uint256 expectedVariableRate = rateData.baseVariableBorrowRate +
-//       ((rateData.variableRateSlope1 + rateData.variableRateSlope2) * excessBorrowUsageRatio) /
-//       10000;
-
-//     assertEq(expectedVariableRate.bpsToRay(), variableBorrowRate, 'Invalid borrow rate');
-  
-}
+    assertEq(expectedVariableRate.bpsToRay(), variableBorrowRate, 'Invalid borrow rate');
+  }
 
   function test_calculate_interest_rate_below_optimal_usage() public {
-    vm.skip(true, 'pending refactor');
+    IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
+    rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
+    uint256 totalDebt = 4e17;
+    uint256 virtualUnderlyingBalance = 6e17;
 
-//     IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
-//     rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
-//     uint256 totalDebt = 4e17;
-//     uint256 virtualUnderlyingBalance = 6e17;
+    uint256 utilizationRatio = getUtilizationRatio(totalDebt, virtualUnderlyingBalance);
 
-//     uint256 utilizationRatio = getUtilizationRatio(totalDebt, virtualUnderlyingBalance);
+    uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
+      DataTypes.CalculateInterestRatesParams({
+        liquidityAdded: 0,
+        liquidityTaken: 0,
+        totalDebt: totalDebt,
+        reserveFactor: 0,
+        assetId: mockReserveAddress,
+        virtualUnderlyingBalance: virtualUnderlyingBalance,
+        usingVirtualBalance: true
+      })
+    );
 
-//     uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
-//       DataTypes.CalculateInterestRatesParams({
-//         liquidityAdded: 0,
-//         liquidityTaken: 0,
-//         totalDebt: totalDebt,
-//         reserveFactor: 0,
-//         assetId: mockReserveAddress,
-//         virtualUnderlyingBalance: virtualUnderlyingBalance,
-//         usingVirtualBalance: true
-//       })
-//     );
-
-//     uint256 expectedVariableRate = rateData.baseVariableBorrowRate +
-//       (rateData.variableRateSlope1 * utilizationRatio) /
-//       rateData.optimalUsageRatio;
-//     assertEq(
-//       expectedVariableRate.bpsToRay(),
-//       variableBorrowRate,
-//       'Invalid borrow rate below optimal usage'
-//     );
-  
-}
+    uint256 expectedVariableRate = rateData.baseVariableBorrowRate +
+      (rateData.variableRateSlope1 * utilizationRatio) /
+      rateData.optimalUsageRatio;
+    assertEq(
+      expectedVariableRate.bpsToRay(),
+      variableBorrowRate,
+      'Invalid borrow rate below optimal usage'
+    );
+  }
 
   function test_calculate_interest_rate_zero_debt() public {
-    vm.skip(true, 'pending refactor');
+    IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
+    rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
 
-//     IDefaultInterestRateStrategy.InterestRateData memory rateData = _getMockInterestRateData();
-//     rateStrategy.setInterestRateParams(mockReserveAddress, rateData);
+    uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
+      DataTypes.CalculateInterestRatesParams({
+        liquidityAdded: 0,
+        liquidityTaken: 0,
+        totalDebt: 0,
+        reserveFactor: 0,
+        assetId: mockReserveAddress,
+        virtualUnderlyingBalance: 1e18,
+        usingVirtualBalance: true
+      })
+    );
 
-//     uint256 variableBorrowRate = rateStrategy.calculateInterestRates(
-//       DataTypes.CalculateInterestRatesParams({
-//         liquidityAdded: 0,
-//         liquidityTaken: 0,
-//         totalDebt: 0,
-//         reserveFactor: 0,
-//         assetId: mockReserveAddress,
-//         virtualUnderlyingBalance: 1e18,
-//         usingVirtualBalance: true
-//       })
-//     );
-
-//     assertEq(
-//       variableBorrowRate,
-//       rateData.baseVariableBorrowRate,
-//       'Invalid borrow rate with zero debt'
-//     );
-  
-}
+    assertEq(
+      variableBorrowRate,
+      rateData.baseVariableBorrowRate,
+      'Invalid borrow rate with zero debt'
+    );
+  }
 }
