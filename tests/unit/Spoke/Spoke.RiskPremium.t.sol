879a37f9c0ac84c9d231024ab47047f91e7bc498
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-04-25 12:02:47 -0700

    chore: Rename test

--- tests/unit/Spoke/Spoke.RiskPremium.t.sol
+++ tests/unit/Spoke/Spoke.RiskPremium.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import 'tests/unit/Spoke/SpokeBase.t.sol';
 
-contract SpokeUserRiskPremiumTest is SpokeBase {
+contract SpokeRiskPremiumTest is SpokeBase {
   using SharesMath for uint256;
   using WadRayMath for uint256;
   using PercentageMath for uint256;
