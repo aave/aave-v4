794b0cbfd2af0adba18e41f0d83f6f82a9ed9078
YBM <yan@avara.xyz>
2025-04-25 15:05:18 -0500

    chore: opt LiquidationLogicActualDebtToLiquidateTest

--- tests/unit/libraries/LiquidationLogic/LiquidationLogic.ActualDebtToLiquidate.t.sol
+++ tests/unit/libraries/LiquidationLogic/LiquidationLogic.ActualDebtToLiquidate.t.sol
@@ -4,6 +4,19 @@ pragma solidity ^0.8.0;
 import 'tests/unit/libraries/LiquidationLogic/LiquidationLogic.Base.t.sol';
 
 contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
+  function _bound(
+    TestDebtToRestoreCloseFactorParams memory params,
+    FieldsToSkip memory skip
+  ) internal override returns (TestDebtToRestoreCloseFactorParams memory) {
+    params = super._bound(params, skip);
+    params.totalDebt = bound(
+      params.totalDebt,
+      1,
+      MAX_TOTAL_ASSET_IN_BASE_CURRENCY / params.debtAssetUnit
+    );
+    return params;
+  }
+
   function test_calculateActualDebtToLiquidate_fuzz_totalDebt_zero(
     uint256 debtToCover,
     TestDebtToRestoreCloseFactorParams memory params
@@ -22,11 +35,14 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
       args
     );
 
-    assertEq(actualDebtToLiquidate, 0, 'if totalDebt == 0, actualDebtToLiquidate should be 0');
+    assertEq(
+      actualDebtToLiquidate,
+      0,
+      'if debtToRestoreCloseFactor == 0, actualDebtToLiquidate should be 0'
+    );
   }
 
   function test_calculateActualDebtToLiquidate_fuzz_debtToCover_zero(
-    uint256 totalDebt,
     TestDebtToRestoreCloseFactorParams memory params
   ) public {
     FieldsToSkip memory skips = _skipOnly(SKIP_NONE);
@@ -35,7 +51,6 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
 
     // zero debtToCover; should be reverted by validation in practice
     uint256 debtToCover = 0;
-    args.totalDebt = totalDebt;
 
     uint256 actualDebtToLiquidate = LiquidationLogic.calculateActualDebtToLiquidate(
       debtToCover,
@@ -45,9 +60,8 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
     assertEq(actualDebtToLiquidate, 0, 'if debtToCover == 0, actualDebtToLiquidate should be 0');
   }
 
-  function test_calculateActualDebtToLiquidate_fuzz_totalDebt_gt_debtToRestoreCloseFactor(
+  function test_calculateActualDebtToLiquidate_fuzz_debtToRestoreCloseFactor_lte_totalDebt(
     uint256 debtToCover,
-    uint256 totalDebt,
     TestDebtToRestoreCloseFactorParams memory params
   ) public {
     FieldsToSkip memory skips = _skipOnly(SKIP_NONE);
@@ -56,33 +70,24 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
 
     uint256 debtToRestoreCloseFactor = LiquidationLogic.calculateDebtToRestoreCloseFactor(args);
 
-    // totalDebt > debtToRestoreCloseFactor
-    totalDebt = bound(totalDebt, debtToRestoreCloseFactor, type(uint256).max);
-    args.totalDebt = totalDebt;
+    vm.assume(debtToRestoreCloseFactor > args.totalDebt);
 
     uint256 actualDebtToLiquidate = LiquidationLogic.calculateActualDebtToLiquidate(
       debtToCover,
       args
     );
 
-    if (debtToCover > debtToRestoreCloseFactor) {
-      assertEq(
-        actualDebtToLiquidate,
-        debtToRestoreCloseFactor,
-        'debtToCover > debtToRestoreCloseFactor, should return debtToRestoreCloseFactor'
-      );
-    } else {
-      assertEq(
-        actualDebtToLiquidate,
-        debtToCover,
-        'debtToCover <= debtToRestoreCloseFactor, should return debtToCover'
-      );
-    }
+    uint256 maxLiquidatableDebt = _min(debtToRestoreCloseFactor, args.totalDebt);
+
+    assertEq(
+      actualDebtToLiquidate,
+      _min(debtToCover, maxLiquidatableDebt),
+      'should return min allowed'
+    );
   }
 
   function test_calculateActualDebtToLiquidate_fuzz_debtToRestoreCloseFactor_gt_maxLiquidatableDebt(
     uint256 debtToCover,
-    uint256 totalDebt,
     TestDebtToRestoreCloseFactorParams memory params
   ) public {
     FieldsToSkip memory skips = _skipOnly(SKIP_NONE);
@@ -102,13 +107,12 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
     assertEq(
       actualDebtToLiquidate,
       _min(debtToCover, maxLiquidatableDebt),
-      'debtToCover > totalDebt, should return totalDebt'
+      'should return min allowed'
     );
   }
 
   function test_calculateActualDebtToLiquidate_fuzz_debtToRestoreCloseFactor_zero(
     uint256 debtToCover,
-    uint256 totalDebt,
     TestDebtToRestoreCloseFactorParams memory params
   ) public {
     FieldsToSkip memory skips = _skipOnly(SKIP_NONE);
@@ -117,8 +121,6 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
 
     uint256 debtToRestoreCloseFactor = LiquidationLogic.calculateDebtToRestoreCloseFactor(args);
     vm.assume(debtToRestoreCloseFactor == 0);
-
-    args.totalDebt = totalDebt;
 
     uint256 actualDebtToLiquidate = LiquidationLogic.calculateActualDebtToLiquidate(
       debtToCover,
@@ -130,7 +132,6 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
 
   function test_calculateActualDebtToLiquidate_fuzz_debtToRestoreCloseFactor_min(
     uint256 debtToCover,
-    uint256 totalDebt,
     TestDebtToRestoreCloseFactorParams memory params
   ) public {
     FieldsToSkip memory skips = _skipOnly(SKIP_NONE);
@@ -139,8 +140,6 @@ contract LiquidationLogicActualDebtToLiquidateTest is LiquidationLogicBaseTest {
 
     uint256 debtToRestoreCloseFactor = LiquidationLogic.calculateDebtToRestoreCloseFactor(args);
     vm.assume(debtToRestoreCloseFactor == 0);
-
-    args.totalDebt = totalDebt;
 
     uint256 actualDebtToLiquidate = LiquidationLogic.calculateActualDebtToLiquidate(
       debtToCover,
