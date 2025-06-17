481963fe5780a611de5ef624d7e08456efc8fea9
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-06-16 19:33:20 -0700

    test: Showcase granularity of roles

--- src/libraries/types/Roles.sol
+++ src/libraries/types/Roles.sol
@@ -2,5 +2,6 @@
 pragma solidity ^0.8.10;
 
 library Roles {
+  uint64 public constant ADMIN_ROLE = 0;
   uint64 public constant RESTRICTED_ROLE = 1;
 }
--- tests/unit/Spoke/Spoke.Access.t.sol
+++ tests/unit/Spoke/Spoke.Access.t.sol
@@ -30,4 +30,57 @@ contract SpokeBorrowTest is SpokeBase {
     vm.prank(bob);
     spoke1.restrictedFunction();
   }
+
+  /// @dev Bob has the role to call the restricted function, but we change what the role can do
+  function testAccess_revoke_role_ability() public {
+    // Grant Bob the role to call the restricted function
+    bytes4[] memory selectors = new bytes4[](1);
+    selectors[0] = ISpoke.restrictedFunction.selector;
+
+    vm.startPrank(HUB_ADMIN);
+    accessManager.setTargetFunctionRole(address(spoke1), selectors, Roles.RESTRICTED_ROLE);
+    accessManager.grantRole(Roles.RESTRICTED_ROLE, bob, 0);
+    vm.stopPrank();
+
+    // Bob can call the restricted function
+    vm.prank(bob);
+    spoke1.restrictedFunction();
+
+    // Now we change what the role can do, removing the ability to call the restricted function
+    vm.startPrank(HUB_ADMIN);
+    accessManager.setTargetFunctionRole(address(spoke1), selectors, Roles.ADMIN_ROLE);
+    vm.stopPrank();
+
+    // Bob should no longer be able to call the restricted function
+    vm.expectRevert(abi.encodeWithSelector(IAccessManaged.AccessManagedUnauthorized.selector, bob));
+    vm.prank(bob);
+    spoke1.restrictedFunction();
+  }
+
+  /// @dev Bob has the role to call the restricted function, but then we remove the role from Bob
+  function testAccess_revoke_role_from_bob() public {
+    // Grant Bob the role to call the restricted function
+    bytes4[] memory selectors = new bytes4[](1);
+    selectors[0] = ISpoke.restrictedFunction.selector;
+    vm.startPrank(HUB_ADMIN);
+    accessManager.setTargetFunctionRole(address(spoke1), selectors, Roles.RESTRICTED_ROLE);
+    accessManager.grantRole(Roles.RESTRICTED_ROLE, bob, 0);
+    vm.stopPrank();
+
+    // Bob can call the restricted function
+    vm.prank(bob);
+    spoke1.restrictedFunction();
+
+    // Now we remove the role from Bob
+    vm.startPrank(HUB_ADMIN);
+    accessManager.revokeRole(Roles.RESTRICTED_ROLE, bob);
+    vm.stopPrank();
+
+    // Bob should no longer be able to call the restricted function
+    vm.expectRevert(abi.encodeWithSelector(IAccessManaged.AccessManagedUnauthorized.selector, bob));
+    vm.prank(bob);
+    spoke1.restrictedFunction();
+  }
+
+  // TODO: Showcase Alice being an admin of the restricted role
 }
