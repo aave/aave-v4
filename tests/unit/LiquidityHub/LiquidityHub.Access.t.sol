34c54a21797dbe645510fb4957d0a728fcd0834e
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-06-27 21:46:55 -0700

    test: Migrate role

--- tests/unit/LiquidityHub/LiquidityHub.Access.t.sol
+++ tests/unit/LiquidityHub/LiquidityHub.Access.t.sol
@@ -160,6 +160,108 @@ contract LiquidityHubAccessTest is LiquidityHubBase {
     hub.updateAssetFees(daiAssetId, address(0), 0);
   }
 
+  /// @dev Test showcasing ability to migrate role responsibility for a function selector.
+  function test_migrate_role_responsibility() public {
+    bytes memory encodedIrData = abi.encode(
+      IAssetInterestRateStrategy.InterestRateData({
+        optimalUsageRatio: 50_00, // 50.00% in BPS
+        baseVariableBorrowRate: 100_00, // 100.00% in BPS
+        variableRateSlope1: 200_00, // 200.00% in BPS
+        variableRateSlope2: 300_00 // 300.00% in BPS
+      })
+    );
+
+    // Say addresses Alice, Bob, and Carol all have the HUB_ADMIN role, allowing them to set interest rate data.
+    // Grant roles with 0 delay
+    vm.startPrank(ADMIN);
+    accessManager.grantRole(Roles.HUB_ADMIN_ROLE, alice, 0);
+    accessManager.grantRole(Roles.HUB_ADMIN_ROLE, bob, 0);
+    accessManager.grantRole(Roles.HUB_ADMIN_ROLE, carol, 0);
+    vm.stopPrank();
+
+    vm.prank(alice);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.prank(bob);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.prank(carol);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+
+    // Now, we change the role responsible for setting interest rate data to SPOKE_ADMIN role.
+    bytes4[] memory hubSelectors = new bytes4[](1);
+    hubSelectors[0] = ILiquidityHub.setInterestRateData.selector;
+    vm.prank(ADMIN);
+    accessManager.setTargetFunctionRole(address(hub), hubSelectors, Roles.SPOKE_ADMIN_ROLE);
+
+    // Alice, Bob, and Carol should no longer have access to set interest rate data.
+    vm.expectRevert(
+      abi.encodeWithSelector(IAccessManaged.AccessManagedUnauthorized.selector, alice)
+    );
+    vm.prank(alice);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.expectRevert(abi.encodeWithSelector(IAccessManaged.AccessManagedUnauthorized.selector, bob));
+    vm.prank(bob);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.expectRevert(
+      abi.encodeWithSelector(IAccessManaged.AccessManagedUnauthorized.selector, carol)
+    );
+    vm.prank(carol);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+
+    // Now, we grant SPOKE_ADMIN role to Alice, Bob, and Carol with 0 delay
+    vm.startPrank(ADMIN);
+    accessManager.grantRole(Roles.SPOKE_ADMIN_ROLE, alice, 0);
+    accessManager.grantRole(Roles.SPOKE_ADMIN_ROLE, bob, 0);
+    accessManager.grantRole(Roles.SPOKE_ADMIN_ROLE, carol, 0);
+    vm.stopPrank();
+
+    // Alice, Bob, and Carol should now be able to set interest rate data.
+    vm.prank(alice);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.prank(bob);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.prank(carol);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+
+    // Alice, Bob, and Carol currently have both HUB_ADMIN and SPOKE_ADMIN roles.
+    IAccessManager accessManager = IAccessManager(hub.authority());
+    (bool hasHubAdminRoleAlice, ) = accessManager.hasRole(Roles.HUB_ADMIN_ROLE, alice);
+    (bool hasHubAdminRoleBob, ) = accessManager.hasRole(Roles.HUB_ADMIN_ROLE, bob);
+    (bool hasHubAdminRoleCarol, ) = accessManager.hasRole(Roles.HUB_ADMIN_ROLE, carol);
+    assertTrue(hasHubAdminRoleAlice);
+    assertTrue(hasHubAdminRoleBob);
+    assertTrue(hasHubAdminRoleCarol);
+
+    (bool hasSpokeAdminRoleAlice, ) = accessManager.hasRole(Roles.SPOKE_ADMIN_ROLE, alice);
+    (bool hasSpokeAdminRoleBob, ) = accessManager.hasRole(Roles.SPOKE_ADMIN_ROLE, bob);
+    (bool hasSpokeAdminRoleCarol, ) = accessManager.hasRole(Roles.SPOKE_ADMIN_ROLE, carol);
+    assertTrue(hasSpokeAdminRoleAlice);
+    assertTrue(hasSpokeAdminRoleBob);
+    assertTrue(hasSpokeAdminRoleCarol);
+
+    // We can remove HUB_ADMIN role from Alice, Bob, and Carol.
+    vm.startPrank(ADMIN);
+    accessManager.revokeRole(Roles.HUB_ADMIN_ROLE, alice);
+    accessManager.revokeRole(Roles.HUB_ADMIN_ROLE, bob);
+    accessManager.revokeRole(Roles.HUB_ADMIN_ROLE, carol);
+    vm.stopPrank();
+
+    // Alice, Bob, and Carol should no longer have HUB_ADMIN role.
+    (hasHubAdminRoleAlice, ) = accessManager.hasRole(Roles.HUB_ADMIN_ROLE, alice);
+    (hasHubAdminRoleBob, ) = accessManager.hasRole(Roles.HUB_ADMIN_ROLE, bob);
+    (hasHubAdminRoleCarol, ) = accessManager.hasRole(Roles.HUB_ADMIN_ROLE, carol);
+    assertFalse(hasHubAdminRoleAlice);
+    assertFalse(hasHubAdminRoleBob);
+    assertFalse(hasHubAdminRoleCarol);
+
+    // Can still call setInterestRateData since they have SPOKE_ADMIN role.
+    vm.prank(alice);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.prank(bob);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+    vm.prank(carol);
+    hub.setInterestRateData(daiAssetId, encodedIrData);
+  }
+
   /// @dev Test showcasing authority contract can be accessed via hub contract.
   function test_hub_access_manager_exposure() public {
     assertEq(address(hub.authority()), address(accessManager));
