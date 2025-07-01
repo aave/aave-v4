d7a7b6ab6dfa9daa99201e5c5fd77bff9f7c6129
Alexandru Niculae <43644109+avniculae@users.noreply.github.com>
2025-07-01 09:55:42 +0300

    feat: add natspec

--- src/interfaces/IPriceOracle.sol
+++ src/interfaces/IPriceOracle.sol
@@ -3,16 +3,25 @@ pragma solidity ^0.8.0;
 
 /**
  * @title IPriceOracle
- * @author Aave
- * @notice Defines the basic interface for a Price oracle.
+ * @author Aave Labs
+ * @notice Basic interface for any price oracle used by the Aave protocol
+ * @dev All prices must use the same number of decimals as the oracle and should be returned in the same currency
  */
 interface IPriceOracle {
+  /**
+   * @notice Returns the number of decimals used to return prices
+   * @return The number of decimals
+   */
   function DECIMALS() external view returns (uint256);
 
+  /**
+   * @notice Returns the description of the oracle
+   * @return The description of the oracle
+   */
   function DESCRIPTION() external view returns (string memory);
 
   /**
-   * @notice Returns the reserve price in the base currency
+   * @notice Returns the reserve price with `decimals` precision
    * @param reserveId The id of the reserve
    * @return The price of the reserve
    */
