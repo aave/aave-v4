c4c24a048b5cfc8b67646ba0b2e15cdf6678f34c
YBM <31329384+yan-man@users.noreply.github.com>
2025-07-31 22:07:13 -0500

    chore: rm comment

--- src/libraries/math/MathUtils.sol
+++ src/libraries/math/MathUtils.sol
@@ -131,6 +131,6 @@ library MathUtils {
    * @return The signed result of the subtraction.
    */
   function signedSub(uint256 a, uint256 b) internal pure returns (int256) {
-    return int256(a) - int256(b); // todo use safeCast when amounts packed to uint112/uint128
+    return int256(a) - int256(b);
   }
 }
