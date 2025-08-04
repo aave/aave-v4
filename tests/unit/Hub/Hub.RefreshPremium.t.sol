7622700da3f3282e0c97442143b3a68a9deca165
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-08-04 21:47:53 +0200

    test: Positive input refresh premium

--- tests/unit/Hub/Hub.RefreshPremium.t.sol
+++ tests/unit/Hub/Hub.RefreshPremium.t.sol
@@ -28,4 +28,32 @@ contract HubRefreshPremiumTest is HubBase {
     vm.prank(address(spoke1));
     hub1.refreshPremium(daiAssetId, premiumDelta);
   }
+
+  // TODO: Try negative number cases
+  /// @dev offsetDelta can't be more than sharesDelta or else underflow
+  /// @dev sharesDelta can't be more than 2 more than offsetDelta
+  /// @dev realizedDelta can't be more than 2
+  function test_refreshPremium(
+    uint256 sharesDelta,
+    uint256 offsetDelta,
+    uint256 realizedDelta
+  ) public {
+    sharesDelta = bound(sharesDelta, 0, MAX_SUPPLY_AMOUNT);
+    offsetDelta = bound(offsetDelta, 0, MAX_SUPPLY_AMOUNT);
+    realizedDelta = bound(realizedDelta, 0, MAX_SUPPLY_AMOUNT);
+    DataTypes.PremiumDelta memory premiumDelta = DataTypes.PremiumDelta({
+      sharesDelta: int256(sharesDelta),
+      offsetDelta: int256(offsetDelta),
+      realizedDelta: int256(realizedDelta)
+    });
+    uint256 assetId = daiAssetId;
+
+    if (offsetDelta > sharesDelta) {
+      vm.expectRevert(stdError.arithmeticError);
+    } else if (sharesDelta > 2 + offsetDelta || realizedDelta > 2) {
+      vm.expectRevert(IHub.InvalidPremiumChange.selector);
+    }
+    vm.prank(address(spoke1));
+    hub1.refreshPremium(assetId, premiumDelta);
+  }
 }
