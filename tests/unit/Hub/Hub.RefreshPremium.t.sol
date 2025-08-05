8da3e74c6e73407ec5bc62931f3fa5c32cf8dea7
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-08-05 18:16:16 +0200

    fix: General refreshPremium test

--- tests/unit/Hub/Hub.RefreshPremium.t.sol
+++ tests/unit/Hub/Hub.RefreshPremium.t.sol
@@ -37,6 +37,7 @@ contract HubRefreshPremiumTest is HubBase {
     uint256 offsetDelta,
     uint256 realizedDelta
   ) public {
+    //DataTypes.Asset memory asset = hub1.getAsset(assetId);
     sharesDelta = bound(sharesDelta, 0, MAX_SUPPLY_AMOUNT);
     offsetDelta = bound(offsetDelta, 0, MAX_SUPPLY_AMOUNT);
     realizedDelta = bound(realizedDelta, 0, MAX_SUPPLY_AMOUNT);
@@ -49,7 +50,7 @@ contract HubRefreshPremiumTest is HubBase {
 
     if (offsetDelta > sharesDelta) {
       vm.expectRevert(stdError.arithmeticError);
-    } else if (sharesDelta > 2 + offsetDelta || realizedDelta > 2) {
+    } else if (sharesDelta - offsetDelta + realizedDelta > 2) {
       vm.expectRevert(IHub.InvalidPremiumChange.selector);
     }
     vm.prank(address(spoke1));
@@ -129,4 +130,32 @@ contract HubRefreshPremiumTest is HubBase {
     vm.prank(address(spoke1));
     hub1.refreshPremium(assetId, premiumDelta);
   }
+
+  /*
+  // TODO: Write a fuzz test with positive or negative numbers, with debt accrual
+  // TODO: Fuzz just 1 number, like sharesDelta, and make the others work around it
+  function test_refreshPremium_fuzz_withAccrual(int256 sharesDelta) public {
+    // Bob supplies and borrows dai via spoke 1
+    uint256 assetId = daiAssetId;
+    Utils.supplyCollateral(spoke1, _daiReserveId(spoke1), bob, 10000e18, bob);
+    Utils.borrow(spoke1, _daiReserveId(spoke1), bob, 5000e18, bob);
+
+    skip(322 days);
+    Utils.borrow(spoke1, _daiReserveId(spoke1), bob, 1e18, bob);
+
+    DataTypes.Asset memory asset = hub1.getAsset(assetId);
+
+    sharesDelta = bound(sharesDelta, -int256(asset.premiumShares), int256(asset.premiumShares));
+    int256 offsetDelta = -int256(asset.premiumOffset); // todo: left off here
+    int256 realizedDelta = -int256(asset.realizedPremium);
+    DataTypes.PremiumDelta memory premiumDelta = DataTypes.PremiumDelta({
+      sharesDelta: -int256(sharesDelta),
+      offsetDelta: offsetDelta,
+      realizedDelta: realizedDelta
+    });
+
+    vm.prank(address(spoke1));
+    hub1.refreshPremium(assetId, premiumDelta);
+  }
+  */
 }
