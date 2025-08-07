9b3523bd00a5a7839e7a66f90c1a8c372b5c0f72
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2025-08-07 17:11:48 +0530

    chore: rename smt script

--- tests/misc/liquidity_growth.py
+++ tests/misc/liquidity_growth.py
@@ -13,11 +13,8 @@ s.add(RAY <= index1, index1 < index2, index2 <= 100 * RAY)
 s.add(0 <= base, base <= 10**30)
 s.add(0 <= premium, premium <= 10**30)
 
-def rayMulUp(a, b):
-    return (a * b + RAY - 1) / RAY
-
-def rayMulDown(a, b):
-    return (a * b) / RAY
+rayMulUp = lambda a, b: (a * b + RAY - 1) / RAY
+rayMulDown = lambda a, b: (a * b) / RAY
 
 expected = rayMulUp(base, index2) - rayMulUp(base, index1) + rayMulUp(premium, index2) - rayMulUp(premium, index1)
 actual = rayMulDown(base, index2 - index1) + rayMulDown(premium, index2 - index1)
