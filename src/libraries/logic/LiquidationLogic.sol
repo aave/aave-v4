0df9f6335e2be25726200c5be14f41a9472a1303
YBM <31329384+yan-man@users.noreply.github.com>
2025-08-11 18:38:21 -0500

    fix: rm logging

--- src/libraries/logic/LiquidationLogic.sol
+++ src/libraries/logic/LiquidationLogic.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {console2 as console} from 'forge-std/console2.sol';
 import {DataTypes} from 'src/libraries/types/DataTypes.sol';
 import {PercentageMath} from 'src/libraries/math/PercentageMath.sol';
 import {WadRayMath} from 'src/libraries/math/WadRayMath.sol';
