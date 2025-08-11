7d0fef5403d57313f0049c852a46542adf7ce552
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-08-11 06:34:39 -0700

    Debug: Refresh Premium Tests (#611)
    * wip: Debugging test
    
    * wip: Error found at 100k runs
    
    * test: Fuzz refresh premium
    
    * fix: Comments on hub

--- tests/unit/Hub/Hub.RefreshPremium.t.sol
+++ tests/unit/Hub/Hub.RefreshPremium.t.sol
@@ -135,152 +135,57 @@ contract HubRefreshPremiumTest is HubBase {
   }
 
   function test_refreshPremium_fuzz_withAccrual(
-    int256 sharesDelta,
-    int256 offsetDelta,
-    int256 realizedDelta
+    uint256 borrowAmount,
+    uint256 userPremiumShares,
+    uint256 userAccruedPremium,
+    uint256 userPremiumSharesNew
   ) public {
     uint256 assetId = daiAssetId;
-    Utils.supplyCollateral(spoke1, _daiReserveId(spoke1), bob, 10000e18, bob);
-    Utils.borrow(spoke1, _daiReserveId(spoke1), bob, 5000e18, bob);
+    uint256 skipTime = vm.randomUint(0, MAX_SKIP_TIME);
 
-    skip(322 days);
+    borrowAmount = bound(borrowAmount, 1, MAX_SUPPLY_AMOUNT / 2);
+
+    Utils.supplyCollateral(spoke1, _daiReserveId(spoke1), bob, MAX_SUPPLY_AMOUNT, bob);
+    Utils.borrow(spoke1, _daiReserveId(spoke1), bob, borrowAmount, bob);
+    skip(skipTime);
     Utils.borrow(spoke1, _daiReserveId(spoke1), bob, 1e18, bob);
 
     DataTypes.Asset memory asset = hub1.getAsset(assetId);
 
-    sharesDelta = bound(
-      sharesDelta,
-      -int256(uint256(asset.premiumShares)),
-      int256(MAX_SUPPLY_AMOUNT / 2)
+    // Initial user position
+    userPremiumShares = bound(userPremiumShares, 0, asset.premiumShares);
+    userAccruedPremium = bound(
+      userAccruedPremium,
+      0,
+      hub1.convertToDrawnAssets(assetId, asset.premiumShares) - asset.premiumOffset
     );
-    offsetDelta = bound(
-      offsetDelta,
-      -int256(uint256(asset.premiumOffset)),
-      int256(MAX_SUPPLY_AMOUNT / 2)
-    );
-    realizedDelta = bound(
-      realizedDelta,
-      -int256(MAX_SUPPLY_AMOUNT / 2),
-      int256(MAX_SUPPLY_AMOUNT / 2)
-    );
+    vm.assume(hub1.convertToDrawnAssets(assetId, userPremiumShares) >= userAccruedPremium);
+    uint256 userPremiumOffset = hub1.convertToDrawnAssets(assetId, userPremiumShares) -
+      userAccruedPremium;
+
+    // New user position
+    userPremiumSharesNew = bound(userPremiumSharesNew, 0, MAX_SUPPLY_AMOUNT / 2);
+    uint256 userPremiumOffsetNew = hub1.previewDrawByShares(assetId, userPremiumSharesNew);
 
     DataTypes.PremiumDelta memory premiumDelta = DataTypes.PremiumDelta({
-      sharesDelta: int256(sharesDelta),
-      offsetDelta: int256(offsetDelta),
-      realizedDelta: int256(realizedDelta)
+      sharesDelta: int256(userPremiumSharesNew) - int256(userPremiumShares),
+      offsetDelta: int256(userPremiumOffsetNew) - int256(userPremiumOffset),
+      realizedDelta: int256(userAccruedPremium)
     });
 
     if (
-      (sharesDelta < 0 && -sharesDelta > int256(uint256(asset.premiumShares))) ||
-      (offsetDelta < 0 && -offsetDelta > int256(uint256(asset.premiumOffset))) ||
-      (realizedDelta < 0 && -realizedDelta > int256(uint256(asset.realizedPremium)))
+      premiumDelta.sharesDelta < 0 &&
+      -premiumDelta.sharesDelta > int256(uint256(asset.premiumShares))
+    ) {
+      vm.expectRevert(stdError.arithmeticError);
+    } else if (
+      premiumDelta.offsetDelta < 0 &&
+      -premiumDelta.offsetDelta > int256(uint256(asset.premiumOffset))
     ) {
       vm.expectRevert(stdError.arithmeticError);
     }
-    // TODO: Handle sharesDelta positive and negative cases separately
-    // TODO: If shares delta is negative, that means we need to convert to positive when converting to drawn assets
-    else if (sharesDelta >= 0) {
-      int256 premiumAssetsDelta = int256(hub1.convertToDrawnAssets(assetId, uint256(sharesDelta)));
-      // If we introduced debt with shares vs offset, capture with realized delta
-      if (premiumAssetsDelta > offsetDelta) {
-        premiumDelta.realizedDelta = -int256(premiumAssetsDelta - offsetDelta);
-      } else {
-        premiumDelta.realizedDelta = 0;
-      }
-
-      if (offsetDelta > premiumAssetsDelta) {
-        vm.expectRevert(stdError.arithmeticError);
-      }
-    } else {
-      int256 premiumAssetsDelta = -int256(
-        hub1.convertToDrawnAssets(assetId, uint256(-sharesDelta))
-      );
-
-      // If we introduced debt with shares vs offset, capture with realized delta
-      if (premiumAssetsDelta > offsetDelta) {
-        premiumDelta.realizedDelta = int256(premiumAssetsDelta - offsetDelta);
-      } else {
-        premiumDelta.realizedDelta = 0;
-      }
-
-      // TODO: Make this condition work
-      // Note that we flip these pos numbers to negative
-      if (offsetDelta > premiumAssetsDelta) {
-        premiumDelta.offsetDelta = int256(premiumAssetsDelta);
-        if (-premiumDelta.offsetDelta > int256(uint256(asset.premiumOffset))) {
-          // set both shares diff and offset diff to match offset
-          premiumDelta.sharesDelta = int256(
-            hub1.convertToDrawnShares(assetId, asset.premiumOffset)
-          );
-          premiumDelta.offsetDelta = int256(uint256(asset.premiumOffset));
-        }
-      }
-    }
-
-    /*
-    if (offsetDelta > sharesDelta) {
-      vm.expectRevert(stdError.arithmeticError);
-    } else if (sharesDelta - offsetDelta + realizedDelta > 2) {
-      // TODO: Handle realizedDelta better
-      vm.expectRevert(IHub.InvalidPremiumChange.selector);
-    }
-    */
-
-    console.log(
-      'sharesDelta: %s, offsetDelta: %s, realizedDelta: %s',
-      premiumDelta.sharesDelta >= 0
-        ? uint256(premiumDelta.sharesDelta)
-        : uint256(-premiumDelta.sharesDelta),
-      premiumDelta.offsetDelta >= 0
-        ? uint256(premiumDelta.offsetDelta)
-        : uint256(-premiumDelta.offsetDelta),
-      premiumDelta.realizedDelta >= 0
-        ? uint256(premiumDelta.realizedDelta)
-        : uint256(-premiumDelta.realizedDelta)
-    );
-    if (premiumDelta.sharesDelta < 0) {
-      console.log('sharesDelta negative');
-    }
-    if (offsetDelta < 0) {
-      console.log('offsetDelta negative');
-    }
-    if (realizedDelta < 0) {
-      console.log('realizedDelta negative');
-    }
-
-    console.log('asset.premiumShares: %s', asset.premiumShares);
-    console.log('asset.premiumOffset: %s', asset.premiumOffset);
-    console.log('asset.realizedPremium: %s', asset.realizedPremium);
 
     vm.prank(address(spoke1));
     hub1.refreshPremium(assetId, premiumDelta);
   }
-
-  /*
-  // TODO: Write a fuzz test with positive or negative numbers, with debt accrual
-  // TODO: If I can't generalize, fuzz just 1 number, like sharesDelta, and make the others work around it
-  function test_refreshPremium_fuzz_withAccrual(int256 sharesDelta) public {
-    // Bob supplies and borrows dai via spoke 1
-    uint256 assetId = daiAssetId;
-    Utils.supplyCollateral(spoke1, _daiReserveId(spoke1), bob, 10000e18, bob);
-    Utils.borrow(spoke1, _daiReserveId(spoke1), bob, 5000e18, bob);
-
-    skip(322 days);
-    Utils.borrow(spoke1, _daiReserveId(spoke1), bob, 1e18, bob);
-
-    DataTypes.Asset memory asset = hub1.getAsset(assetId);
-
-    sharesDelta = bound(sharesDelta, -int256(asset.premiumShares), int256(asset.premiumShares));
-    int256 offsetDelta = -int256(asset.premiumOffset); // todo: left off here
-    int256 realizedDelta = -int256(asset.realizedPremium);
-    DataTypes.PremiumDelta memory premiumDelta = DataTypes.PremiumDelta({
-      sharesDelta: -int256(sharesDelta),
-      offsetDelta: offsetDelta,
-      realizedDelta: realizedDelta
-    });
-
-    vm.prank(address(spoke1));
-    hub1.refreshPremium(assetId, premiumDelta);
-  }
-  */
 }
