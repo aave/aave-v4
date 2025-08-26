5fd696e56d51035d5b84662191553e2bb7a50d26
Koga <vigervalentin@gmail.com>
2025-08-26 11:43:59 +0200

    fix : skip unpacking unset values & skip empty items in loop

--- src/libraries/helpers/KeyValueListInMemory.sol
+++ src/libraries/helpers/KeyValueListInMemory.sol
@@ -59,6 +59,8 @@ library KeyValueListInMemory {
   }
 
   function unpack(uint256 data) internal pure returns (uint256, uint256) {
+    // @dev no need to unpack data that was never packed
+    if(data == 0) return(0,0);
     return (unpackKey(data), unpackValue(data));
   }
 }
