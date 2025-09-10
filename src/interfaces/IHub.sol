c7bc58a196e519f6a0cd6c257c84fb37e578f713
YBM <31329384+yan-man@users.noreply.github.com>
2025-09-10 13:19:54 -0500

    fix: pr comments

--- src/interfaces/IHub.sol
+++ src/interfaces/IHub.sol
@@ -111,7 +111,7 @@ interface IHub is IHubBase, IAccessManaged {
 
   /**
    * @notice Updates the configuration of an asset.
-   * @dev If the fee receiver is updated, it is added as a new spoke with maximum add cap and zero draw cap, so it cannot already exist beforehand.
+   * @dev If the fee receiver is updated, it is added as a new spoke with maximum add cap and zero draw cap.
    * @param assetId The identifier of the asset.
    * @param config The new configuration for the asset.
    */
