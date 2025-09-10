4c032ee33393ffe50ba3bd09be708e527e463c44
miguelmtzinf <miguelmtz.mail@gmail.com>
2025-09-10 18:46:33 +0200

    docs: fix docs of setUserPosition fns

--- src/interfaces/ISpoke.sol
+++ src/interfaces/ISpoke.sol
@@ -204,18 +204,19 @@ interface ISpoke is ISpokeBase, IMulticall, IAccessManaged {
   function updateUserDynamicConfig(address onBehalfOf) external;
 
   /**
-   * @notice Allows caller to approve or revoke approval for positionManager.
+   * @notice Enables a user to grant or revoke approval for a position manager
    * @param positionManager The address of the position manager.
-   * @param approve True if user wants to approve position manager, false otherwise.
+   * @param approve True to approve the position manager, false to revoke approval.
    */
   function setUserPositionManager(address positionManager, bool approve) external;
 
   /**
-   * @notice Allows caller to approve or revoke approval for positionManager using a signature.
+   * @notice Enables a user to grant or revoke approval for a position manager using an EIP712-compliant signature.
    * @param positionManager The address of the position manager.
    * @param user The address of the user on whose behalf position manager can act.
-   * @param approve True if user wants to approve position manager, false otherwise.
+   * @param approve True to approve the position manager, false to revoke approval.
    * @param deadline The deadline for the signature.
+   * @param signature The EIP712-compliant signature bytes.
    */
   function setUserPositionManagerWithSig(
     address positionManager,
