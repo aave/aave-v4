04340a22e9b6800236effd41079c1a91fcf307e0
YBM <31329384+yan-man@users.noreply.github.com>
2025-09-12 15:30:04 -0500

    fix: natspec, rm unused error

--- src/interfaces/INativeTokenGateway.sol
+++ src/interfaces/INativeTokenGateway.sol
@@ -5,41 +5,31 @@ pragma solidity ^0.8.0;
 import {IRescuable} from 'src/interfaces/IRescuable.sol';
 
 interface INativeTokenGateway is IRescuable {
-  /**
-   * @notice Thrown when the given address is invalid.
-   */
+  /// @notice Thrown when the given address is invalid.
   error InvalidAddress();
-  /**
-   * @notice Thrown when the given amount is invalid.
-   */
+
+  /// @notice Thrown when the given amount is invalid.
   error InvalidAmount();
-  /**
-   * @notice Thrown when the given reserveId does not match the wrapped native asset reserveId on the Spoke.
-   */
+
+  /// @notice Thrown when the given reserveId does not match the wrapped native asset reserveId on the Spoke.
   error InvalidReserveId();
-  /**
-   * @notice Thrown when the native amount sent does not match the given amount parameter.
-   */
+
+  /// @notice Thrown when the native amount sent does not match the given amount parameter.
   error NativeAmountMismatch();
-  /**
-   * @notice Thrown when a native transfer fails.
-   */
-  error NativeTransferFailed();
-  /**
-   * @notice Thrown when trying to call an unsupported action or sending native assets to this contract directly.
-   */
+
+  /// @notice Thrown when trying to call an unsupported action or sending native assets to this contract directly.
   error UnsupportedAction();
 
   /**
-   * @notice Wraps the native asset and supply in the Spoke.
-   * @param reserveId Reserve Id for the wrapped asset.
+   * @notice Wraps the native asset and supplies to the Spoke.
+   * @param reserveId The identifier of the reserve for the wrapped asset.
    * @param amount Amount to wrap and supply.
    **/
   function supplyNative(uint256 reserveId, uint256 amount) external payable;
 
   /**
    * @notice Withdraws the wrapped asset from the Spoke and unwraps it back to the native asset.
-   * @param reserveId Reserve Id for the wrapped asset.
+   * @param reserveId The identifier of the reserve for the wrapped asset.
    * @param amount Amount to withdraw and unwrap.
    * @param receiver Address that will receive the unwrapped native asset.
    **/
@@ -47,7 +37,7 @@ interface INativeTokenGateway is IRescuable {
 
   /**
    * @notice Borrows the wrapped asset from the Spoke and unwraps it back to the native asset.
-   * @param reserveId Reserve Id for the wrapped asset.
+   * @param reserveId The identifier of the reserve for the wrapped asset.
    * @param amount Amount to borrow and unwrap.
    * @param receiver Address that will receive the unwrapped native asset.
    **/
@@ -55,7 +45,7 @@ interface INativeTokenGateway is IRescuable {
 
   /**
    * @notice Wraps the native asset and repay debt on the Spoke.
-   * @param reserveId Reserve Id for the wrapped asset.
+   * @param reserveId The identifier of the reserve for the wrapped asset.
    * @param amount Amount to wrap and repay.
    **/
   function repayNative(uint256 reserveId, uint256 amount) external payable;
