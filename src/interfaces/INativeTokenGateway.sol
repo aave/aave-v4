ca53ae0992edce0e5e380f1fc2ed4da8537c37f2
YBM <31329384+yan-man@users.noreply.github.com>
2025-09-12 15:55:03 -0500

    fix: natspec

--- src/interfaces/INativeTokenGateway.sol
+++ src/interfaces/INativeTokenGateway.sol
@@ -6,28 +6,28 @@ import {IRescuable} from 'src/interfaces/IRescuable.sol';
 
 interface INativeTokenGateway is IRescuable {
   /**
-  @notice Thrown when the given address is invalid.
-  */
+   * @notice Thrown when the given address is invalid.
+   */
   error InvalidAddress();
 
   /**
-  @notice Thrown when the given amount is invalid.
-  */
+   * @notice Thrown when the given amount is invalid.
+   */
   error InvalidAmount();
 
   /**
-  @notice Thrown when the given reserveId does not match the wrapped native asset reserveId on the Spoke.
-  */
+   * @notice Thrown when the given reserveId does not match the wrapped native asset reserveId on the Spoke.
+   */
   error InvalidReserveId();
 
   /**
-  @notice Thrown when the native amount sent does not match the given amount parameter.
-  */
+   * @notice Thrown when the native amount sent does not match the given amount parameter.
+   */
   error NativeAmountMismatch();
 
   /**
-  @notice Thrown when trying to call an unsupported action or sending native assets to this contract directly.
-  */
+   * @notice Thrown when trying to call an unsupported action or sending native assets to this contract directly.
+   */
   error UnsupportedAction();
 
   /**
