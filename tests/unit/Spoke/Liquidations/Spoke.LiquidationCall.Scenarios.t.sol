98a0a84fe7ed25de8600b23e8c2f769ac1849fcc
Alexandru Niculae <43644109+avniculae@users.noreply.github.com>
2025-09-15 13:06:35 +0300

    fix: liquidations when liquidity is low

--- tests/unit/Spoke/Liquidations/Spoke.LiquidationCall.Scenarios.t.sol
+++ tests/unit/Spoke/Liquidations/Spoke.LiquidationCall.Scenarios.t.sol
@@ -27,6 +27,25 @@ contract SpokeLiquidationCallScenariosTest is SpokeLiquidationCallBaseTest {
     _updateCollateralRisk(spoke, _usdxReserveId(spoke), 10_00);
     _updateCollateralRisk(spoke, _daiReserveId(spoke), 12_00);
 
+    _updateMaxLiquidationBonus(spoke, _wethReserveId(spoke), 105_00);
+    _updateMaxLiquidationBonus(spoke, _wbtcReserveId(spoke), 103_00);
+    _updateMaxLiquidationBonus(spoke, _usdxReserveId(spoke), 101_00);
+    _updateMaxLiquidationBonus(spoke, _daiReserveId(spoke), 106_00);
+
+    _updateLiquidationFee(spoke, _wethReserveId(spoke), 10_00);
+    _updateLiquidationFee(spoke, _wbtcReserveId(spoke), 15_00);
+    _updateLiquidationFee(spoke, _usdxReserveId(spoke), 12_00);
+    _updateLiquidationFee(spoke, _daiReserveId(spoke), 10_00);
+
+    _updateLiquidationConfig(
+      spoke,
+      ISpoke.LiquidationConfig({
+        targetHealthFactor: _getTargetHealthFactor(spoke),
+        healthFactorForMaxBonus: 0.99e18,
+        liquidationBonusFactor: 100_00
+      })
+    );
+
     for (uint256 reserveId = 0; reserveId < spoke.getReserveCount(); reserveId++) {
       deal(spoke, reserveId, liquidator, MAX_SUPPLY_AMOUNT);
       Utils.approve(hub1, spoke.getReserve(reserveId).assetId, liquidator, MAX_SUPPLY_AMOUNT);
@@ -39,19 +58,8 @@ contract SpokeLiquidationCallScenariosTest is SpokeLiquidationCallBaseTest {
   function test_scenario1() public {
     // A high liquidation bonus will be applied
     _updateMaxLiquidationBonus(spoke, _wethReserveId(spoke), 124_00);
-    _updateLiquidationConfig(
-      spoke,
-      ISpoke.LiquidationConfig({
-        targetHealthFactor: _getTargetHealthFactor(spoke),
-        healthFactorForMaxBonus: 0.99e18,
-        liquidationBonusFactor: 100_00
-      })
-    );
 
     // Borrow rates:
-    //   - WETH: 0%
-    //   - WBTC: 0%
-    //   - USDX: 0%
     //   - DAI: 3%
     vm.prank(address(hub1));
     irStrategy.setInterestRateData(
@@ -102,7 +110,7 @@ contract SpokeLiquidationCallScenariosTest is SpokeLiquidationCallBaseTest {
       'pre liquidation: health factor after 1 year'
     );
 
-    // Debt to target: $3713.4 * (1.05 - 0.97539) / ($1 * (1.05 - 1.24 * 0.8)) = ~$4776.84
+    // Debt to target: $3713.4 * (1.05 - 0.97539) / ($1 * (1.05 - 1.24 * 0.8)) = ~4776.84
     // Liquidation Parameters:
     //   - Collateral: WETH
     //   - Debt: DAI
@@ -112,8 +120,6 @@ contract SpokeLiquidationCallScenariosTest is SpokeLiquidationCallBaseTest {
     //   - Debt: $4000 / ($1 * 1.249) = ~3225.8 DAI
     vm.prank(liquidator);
     spoke.liquidationCall(_wethReserveId(spoke), _daiReserveId(spoke), user, 4000e18);
-
-    userAccountData = spoke.getUserAccountData(user);
 
     // Debt left after liquidation: 3713.4 - 3225.8 = 487.6 DAI (all drawn)
     assertApproxEqAbs(
@@ -129,6 +135,7 @@ contract SpokeLiquidationCallScenariosTest is SpokeLiquidationCallBaseTest {
       'post liquidation: premium debt left'
     );
     // Health Factor after liquidation: ($500 * 0.7 + $100 * 0.72) / ($3713.4 - $3225.8) = ~0.8654
+    userAccountData = spoke.getUserAccountData(user);
     assertApproxEqAbs(
       userAccountData.healthFactor,
       0.8654e18,
@@ -137,5 +144,69 @@ contract SpokeLiquidationCallScenariosTest is SpokeLiquidationCallBaseTest {
     );
     // Risk Premium after liquidation: ($100 * 10% + 387.5 * 15%) / 487.6 = 13.97%
     assertApproxEqAbs(userAccountData.userRiskPremium, 13_97, 1, 'post liquidation: risk premium');
+  }
+
+  // Same collateral is used for debt and collateral, and has low liquidity. Liquidation succeeds
+  // because debt is repaid before collateral is liquidated.
+  function test_scenario2() public {
+    // Borrow rates:
+    //   - WBTC: 3%
+    vm.prank(address(hub1));
+    irStrategy.setInterestRateData(
+      _wbtcReserveId(spoke),
+      abi.encode(
+        IAssetInterestRateStrategy.InterestRateData({
+          optimalUsageRatio: 90_00,
+          baseVariableBorrowRate: 3_00,
+          variableRateSlope1: 0,
+          variableRateSlope2: 0
+        })
+      )
+    );
+
+    // Collateral and debt composition
+    //   - Collaterals: 0.2 WBTC, 0.5 WETH ($11000)
+    //   - Debts: 0.18 WBTC ($9000)
+    // WBTC liquidity: 0.2 - 0.18 = 0.02
+    _increaseCollateralSupply(spoke, _wethReserveId(spoke), 0.5e18, user);
+    _increaseCollateralSupply(spoke, _wbtcReserveId(spoke), 0.2e8, user);
+    _borrowWithoutHfCheck(spoke, user, _wbtcReserveId(spoke), 0.18e8);
+    assertEq(hub1.getLiquidity(wbtcAssetId), 0.02e8);
+
+    // Health Factor: ($10000 * 0.7 + $1000 * 0.8) / $9000 = ~0.8666
+    ISpoke.UserAccountData memory userAccountData = spoke.getUserAccountData(user);
+    assertApproxEqAbs(
+      userAccountData.healthFactor,
+      0.8666e18,
+      0.0001e18,
+      'pre liquidation: health factor'
+    );
+
+    // Debt to target: $9000 * (1.05 - 0.8666) / ($50000 * (1.05 - 1.03 * 0.7)) = ~0.1003
+    // Liquidation Parameters:
+    //   - Collateral: WBTC
+    //   - Debt: WBTC
+    //   - Debt to cover: 0.12
+    // Liquidated amounts:
+    //   - Collateral: 0.1003 * 1.03 = ~0.1033 WBTC
+    //   - Debt: 0.1003 WBTC
+    vm.prank(liquidator);
+    spoke.liquidationCall(_wbtcReserveId(spoke), _wbtcReserveId(spoke), user, 0.13e8);
+
+    // Debt left after liquidation: 0.18 - 0.1003 = 0.0797 WBTC
+    assertApproxEqAbs(
+      getUserDebt(spoke, user, _wbtcReserveId(spoke)).drawnDebt,
+      0.0797e8,
+      0.0001e8,
+      'post liquidation: drawn debt left'
+    );
+    // Health Factor after liquidation: ((0.2 - 0.1033) * $50000 * 0.7 + $1000 * 0.8) / (0.0797 * $50000) = ~1.05 (target health factor)
+    userAccountData = spoke.getUserAccountData(user);
+    assertApproxEqAbs(
+      userAccountData.healthFactor,
+      1.05e18,
+      0.0001e18,
+      'post liquidation: health factor'
+    );
   }
 }
