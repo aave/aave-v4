df12426f4fa95f62ce881664f36aadfc94668ebb
YBM <31329384+yan-man@users.noreply.github.com>
2025-09-17 15:25:21 -0500

    test: cleanup

--- tests/unit/Spoke/Liquidations/Spoke.Liq.t.sol
+++ tests/unit/Spoke/Liquidations/Spoke.Liq.t.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 
 import 'tests/unit/Spoke/SpokeBase.t.sol';
 
-contract SpokeBorrowScenarioTest is SpokeBase {
+/// POC tests for liquidation with 0 restored debt shares
+contract SpokeLiqTest is SpokeBase {
   address liquidator;
   function setUp() public override {
     super.setUp();
@@ -41,99 +42,79 @@ contract SpokeBorrowScenarioTest is SpokeBase {
     tokenList.dai.approve(address(hub1), MAX_SUPPLY_AMOUNT);
   }
 
+  /// POC test for using restore donations to put user positions closer to possibility of bad debt
+  /// by doing multiple liquidations where <1 full debt share is liquidated
   function test_liq_donation() public {
-    // console.log('test');
-    // assertEq(true, false);
-
+    updateCollateralRisk(spoke1, _wethReserveId(spoke1), 0);
     Utils.supplyCollateral(spoke1, _wethReserveId(spoke1), alice, 100e18, alice);
-    _mockInterestRateBps(500_00);
+    _mockInterestRateBps(1_000_00);
 
     Utils.borrow(spoke1, _daiReserveId(spoke1), alice, 100e18, alice);
 
     skip(365 days);
-    // now debt index is 1.5
-
-    console.log('TEST debt index %e', hub1.getAssetDrawnIndex(_daiReserveId(spoke1)));
-    console.log('TEST supply ex rate %e', hub1.convertToAddedAssets(_daiReserveId(spoke1), 1e30));
-    console.log('liquidator balance before %e', tokenList.dai.balanceOf(liquidator));
+    // now debt index is 11
 
     Utils.supplyCollateral(spoke1, _daiReserveId(spoke1), bob, 1_100e18, bob);
-    _borrowToBeAtHf(spoke1, bob, _daiReserveId(spoke1), 0.75e18);
+    _borrowToBeAtHf(spoke1, bob, _daiReserveId(spoke1), 0.95e18);
 
-    // console.log('%e', spoke1.getUserTotalDebt(_daiReserveId(spoke1), bob));
-    // console.log('%e', spoke1.getUserAccountData(bob).healthFactor);
+    console.log('bob hf before liquidation %e', spoke1.getUserAccountData(bob).healthFactor);
+    console.log('liquidator balance before %e', tokenList.dai.balanceOf(liquidator));
 
-    console.log('hf %e', spoke1.getUserAccountData(bob).healthFactor);
-
-    uint256 numIterations = 10000;
+    uint256 numIterations = 100_000;
     uint256[] memory supplyExRate = new uint256[](numIterations);
 
     uint256 supplyAmountBefore = spoke1.getUserSuppliedAssets(_daiReserveId(spoke1), bob);
     uint256 debtAmountBefore = spoke1.getUserTotalDebt(_daiReserveId(spoke1), bob);
     uint256 debtAmountSharesBefore = spoke1.getUserPosition(_daiReserveId(spoke1), bob).drawnShares;
-
-    console.log('supply ex rate %e', hub1.convertToAddedAssets(_daiReserveId(spoke1), 1e30));
+    uint256 premiumDebtSharesBefore = spoke1
+      .getUserPosition(_daiReserveId(spoke1), bob)
+      .premiumShares;
 
     vm.startPrank(liquidator);
+    tokenList.dai.approve(address(hub1), UINT256_MAX);
     for (uint256 i = 0; i < numIterations; i++) {
-      tokenList.dai.approve(address(hub1), MAX_SUPPLY_AMOUNT);
-      // spoke1.supply(_daiReserveId(spoke1), 1e28, liquidator);
-
-      spoke1.liquidationCall(_daiReserveId(spoke1), _daiReserveId(spoke1), bob, 5); // liquidate less than 1 full debt share
-      // supplyExRate[i] = hub1.convertToAddedAssets(_daiReserveId(spoke1), 1e30);
-      // bool increased = i > 0 && supplyExRate[i] > supplyExRate[i - 1];
-
-      // spoke1.withdraw(_daiReserveId(spoke1), UINT256_MAX, liquidator);
-
-      // console.log('supply ex rate incr?', increased);
-      // console.log('hf %e', spoke1.getUserAccountData(bob).healthFactor);
-      // if (increased) {
-      //   console.log(
-      //     'ex rate %e | delta %e',
-      //     supplyExRate[i],
-      //     stdMath.delta(supplyExRate[i], supplyExRate[i - 1])
-      //   );
-      // }
-      // console.log('supply ex rate %e', hub1.convertToAddedShares(_daiReserveId(spoke1), 1e30));
-      // console.log('liquidity %e', hub1.getAsset(_daiReserveId(spoke1)).liquidity);
-
-      // console.log(
-      //   'TEST supplied shares after %e',
-      //   spoke1.getUserSuppliedShares(_daiReserveId(spoke1), bob)
-      // );
+      spoke1.liquidationCall(_daiReserveId(spoke1), _daiReserveId(spoke1), bob, 10); // liquidate less than 1 full debt share
     }
     vm.stopPrank();
 
     uint256 supplyAmountAfter = spoke1.getUserSuppliedAssets(_daiReserveId(spoke1), bob);
     uint256 debtAmountAfter = spoke1.getUserTotalDebt(_daiReserveId(spoke1), bob);
     uint256 debtAmountSharesAfter = spoke1.getUserPosition(_daiReserveId(spoke1), bob).drawnShares;
-
-    // console.log('TEST BEFORE supplied amt %e debt amt %e', supplyAmountBefore, debtAmountBefore);
-    // console.log('TEST AFTER supplied amt %e debt amt %e', supplyAmountAfter, debtAmountAfter);
-
-    console.log('supply ex rate %e', hub1.convertToAddedAssets(_daiReserveId(spoke1), 1e30));
+    uint256 premiumDebtSharesAfter = spoke1
+      .getUserPosition(_daiReserveId(spoke1), bob)
+      .premiumShares;
 
     console.log(
-      'TEST bob supplied amt before/after %e %e delta: %e',
+      'bob supplied amt before/after liqs %e %e delta: %e',
       supplyAmountBefore,
       supplyAmountAfter,
       supplyAmountBefore - supplyAmountAfter
     );
     console.log(
-      'TEST debt amt before/after %e %e delta: %e',
+      'bob debt amt before/after liqs %e %e delta: %e',
       debtAmountBefore,
       debtAmountAfter,
       debtAmountBefore - debtAmountAfter
     );
-    // console.log(
-    //   'TEST debt shares delta %e',
-    //   stdMath.delta(debtAmountSharesAfter, debtAmountSharesBefore)
-    // );
-
+    console.log(
+      'bob debt shares before/after liqs %e %e delta: %e',
+      debtAmountSharesBefore,
+      debtAmountSharesAfter,
+      debtAmountSharesBefore - debtAmountSharesAfter
+    );
+    console.log(
+      'bob premium debt shares before/after liqs %e %e delta: %e',
+      premiumDebtSharesBefore,
+      premiumDebtSharesAfter,
+      premiumDebtSharesBefore - premiumDebtSharesAfter
+    );
+    console.log('bob hf after series of liqs %e', spoke1.getUserAccountData(bob).healthFactor);
     console.log('liquidator balance after %e', tokenList.dai.balanceOf(liquidator));
-    console.log('hf %e', spoke1.getUserAccountData(bob).healthFactor);
 
-    // console.log(spoke1.getUserTotalDebt(_daiReserveId(spoke1), alice));
-    // console.log(hub1.getAssetDrawnIndex(_daiReserveId(spoke1)));
+    /// after liquidations:
+    /// bob's debt has decreased by 0
+    /// bob's collateral value has reduced
+    /// bob's HF has decreased
+    /// liquidator balance remains unchanged, but spent gas to execute these actions
   }
 }
