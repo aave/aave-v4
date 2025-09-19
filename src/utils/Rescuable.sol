130b75876ca62556a251444cdcb02d801a0aa618
YBM <31329384+yan-man@users.noreply.github.com>
2025-09-19 12:52:47 -0500

    natspec: utils

--- src/utils/Rescuable.sol
+++ src/utils/Rescuable.sol
@@ -7,9 +7,17 @@ import {Address} from 'src/dependencies/openzeppelin/Address.sol';
 import {IERC20} from 'src/dependencies/openzeppelin/IERC20.sol';
 import {IRescuable} from 'src/interfaces/IRescuable.sol';
 
+/**
+ * @title Rescuable
+ * @author Aave Labs
+ * @notice Contract that allows for the rescue of tokens and native assets.
+ */
 abstract contract Rescuable is IRescuable {
   using SafeERC20 for IERC20;
 
+  /**
+   * @notice Modifier that checks if the caller is the rescue guardian.
+   */
   modifier onlyRescueGuardian() {
     _checkRescueGuardian();
     _;
@@ -30,8 +38,16 @@ abstract contract Rescuable is IRescuable {
     return _rescueGuardian();
   }
 
+  /**
+   * @notice Returns the rescue guardian address.
+   * @return The address allowed to rescue funds.
+   **/
   function _rescueGuardian() internal view virtual returns (address);
 
+  /**
+   * @notice Checks if the caller is the rescue guardian.
+   * @dev Reverts if the caller is not the rescue guardian.
+   */
   function _checkRescueGuardian() internal view virtual {
     require(_rescueGuardian() == msg.sender, OnlyRescueGuardian());
   }
