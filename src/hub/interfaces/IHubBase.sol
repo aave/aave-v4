8b8c7290b5ee105463e865fa685321de7e23a18c
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-09-30 13:10:06 -0700

    Fix: getSpokeAddedShares natspec (#812)

--- src/hub/interfaces/IHubBase.sol
+++ src/hub/interfaces/IHubBase.sol
@@ -357,6 +357,7 @@ interface IHubBase {
 
   /**
    * @notice Returns the total amount of shares of the specified asset added to the hub by the specified spoke.
+   * @dev If spoke is `asset.feeReceiver`, includes converted `unrealizedFeeShares` in return value.
    * @param assetId The identifier of the asset.
    * @param spoke The address of the spoke.
    * @return The amount of added shares.
