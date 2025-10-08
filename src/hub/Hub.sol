f56ea73cf42ce5693162cf045a855c4106b7e533
miguelmtzinf <miguelmtz.mail@gmail.com>
2025-10-08 17:24:52 +0200

    fix: fix·

--- src/hub/Hub.sol
+++ src/hub/Hub.sol
@@ -324,8 +324,8 @@ contract Hub is IHub, AccessManaged {
     SpokeData storage coveredSpoke = _spokes[assetId][spoke];
 
     _assets.accrue(_spokes, assetId);
-    uint256 deficit = coveredSpoke.deficit;
     _validateEliminateDeficit(callerSpoke, amount);
+    uint256 deficit = coveredSpoke.deficit;
     require(amount <= deficit, InvalidAmount());
 
     uint128 shares = previewRemoveByAssets(assetId, amount).toUint128();
