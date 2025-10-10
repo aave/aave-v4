cc23caf05dc34019eb998aca090bed60c0ba67a2
YBM <31329384+yan-man@users.noreply.github.com>
2025-10-10 10:59:22 -0400

    fix: revert

--- src/spoke/Spoke.sol
+++ src/spoke/Spoke.sol
@@ -230,9 +230,9 @@ abstract contract Spoke is ISpoke, Multicall, NoncesKeyed, AccessManagedUpgradea
 
     uint256 newRiskPremium;
     if (_positionStatus[onBehalfOf].isUsingAsCollateral(reserveId)) {
-      newRiskPremium = _calculateUserAccountData(onBehalfOf).riskPremium;
-    } else {
       newRiskPremium = _refreshAndValidateUserPosition(onBehalfOf);
+    } else {
+      newRiskPremium = _calculateUserAccountData(onBehalfOf).riskPremium;
     }
     _notifyRiskPremiumUpdate(onBehalfOf, newRiskPremium);
 
