0ba004af2b9e56cd560d0d7d8ed65ee0da4d5c31
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2025-10-11 13:51:51 +0530

    opt: avoid mem expansion on safe transfer

--- tests/Base.t.sol
+++ tests/Base.t.sol
@@ -84,6 +84,8 @@ abstract contract Base is Test {
   using SafeCast for *;
   using MathUtils for uint256;
 
+  error TransferFromFailed();
+
   bytes32 internal constant ERC1967_ADMIN_SLOT =
     0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
   bytes32 internal constant IMPLEMENTATION_SLOT =
