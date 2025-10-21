7ef5b9e9a73cb7cbdc62ea0890e6e2efc1988cfd
Cheyenne Atapour <cheyenneatapour@gmail.com>
2025-10-21 12:40:41 -0700

    fix: Revert check on remove

--- src/hub/Hub.sol
+++ src/hub/Hub.sol
@@ -741,8 +741,6 @@ contract Hub is IHub, AccessManaged {
     require(amount > 0, InvalidAmount());
     require(spoke.active, SpokeNotActive());
     require(!spoke.paused, SpokePaused());
-    uint256 removable = asset.toAddedAssetsDown(spoke.addedShares);
-    require(amount <= removable, AddedAmountExceeded(removable));
   }
 
   /// @dev Spoke with maximum cap have unlimited draw capacity.
