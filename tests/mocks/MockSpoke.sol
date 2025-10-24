00f244f53b15c20ec8d4e66c75194b100cf62ec9
miguelmtzinf <miguelmtz.mail@gmail.com>
2025-10-24 11:44:29 +0200

    fix: fix mock

--- tests/mocks/MockSpoke.sol
+++ tests/mocks/MockSpoke.sol
@@ -44,7 +44,7 @@ contract MockSpoke is Spoke, Test {
     userPosition.drawnShares += drawnShares.toUint128();
     positionStatus.setBorrowing(reserveId, true);
 
-    ISpoke.UserAccountData memory userAccountData = _calculateAndValidateUserPosition(onBehalfOf);
+    ISpoke.UserAccountData memory userAccountData = _calculateUserAccountData(onBehalfOf, true);
     _notifyRiskPremiumUpdate(onBehalfOf, userAccountData.riskPremium);
 
     emit Borrow(reserveId, msg.sender, onBehalfOf, drawnShares);
