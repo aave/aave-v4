41a7d214012b99a2ebfe0913703c6d2c39ccc52a
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2025-10-24 16:10:14 +0530

    chore: fix compiler warning

--- src/hub/Hub.sol
+++ src/hub/Hub.sol
@@ -219,7 +219,7 @@ contract Hub is IHub, AccessManaged {
     SpokeData storage spoke = _spokes[assetId][msg.sender];
 
     asset.accrue(_spokes, assetId);
-    _validateRemove(asset, spoke, amount, to);
+    _validateRemove(spoke, amount, to);
 
     uint256 liquidity = asset.liquidity;
     require(amount <= liquidity, InsufficientLiquidity(liquidity));
@@ -758,12 +758,7 @@ contract Hub is IHub, AccessManaged {
     );
   }
 
-  function _validateRemove(
-    Asset storage asset,
-    SpokeData storage spoke,
-    uint256 amount,
-    address to
-  ) internal view {
+  function _validateRemove(SpokeData storage spoke, uint256 amount, address to) internal view {
     require(to != address(this), InvalidAddress());
     require(amount > 0, InvalidAmount());
     require(spoke.active, SpokeNotActive());
