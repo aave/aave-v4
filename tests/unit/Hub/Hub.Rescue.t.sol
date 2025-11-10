241d423a235c884e27b19046a2b674cb799f3f24
YBM <31329384+yan-man@users.noreply.github.com>
2025-11-10 09:32:07 -0600

    fix: pr comments

--- tests/unit/Hub/Hub.Rescue.t.sol
+++ tests/unit/Hub/Hub.Rescue.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import 'tests/unit/Hub/HubBase.t.sol';
 
 contract HubRescueTest is HubBase {
-  address _rescueSpoke;
+  address internal _rescueSpoke;
 
   function setUp() public override {
     super.setUp();
