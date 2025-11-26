dbba30a924f5657b509fe7fe70146cdb3df2d92a
Kogaroshi <vigervalentin@gmail.com>
2025-11-26 17:20:47 +0100

    fix : fix comments

--- src/spoke/libraries/ReserveFlagsMap.sol
+++ src/spoke/libraries/ReserveFlagsMap.sol
@@ -10,20 +10,20 @@ using ReserveFlagsMap for ReserveFlags global;
 /// @author Aave Labs
 /// @notice Implements the bitmap logic to handle the Reserve flags configuration.
 library ReserveFlagsMap {
-  /// @dev Mask for the 'paused' flag.
+  /// @dev Mask for the `paused` flag.
   uint8 internal constant PAUSED_MASK = 0x01;
-  /// @dev Mask for the 'frozen' flag.
+  /// @dev Mask for the `frozen` flag.
   uint8 internal constant FROZEN_MASK = 0x02;
-  /// @dev Mask for the 'borrowable' flag.
+  /// @dev Mask for the `borrowable` flag.
   uint8 internal constant BORROWABLE_MASK = 0x04;
-  /// @dev Mask for the 'receiveSharesEnabled' flag.
+  /// @dev Mask for the `receiveSharesEnabled` flag.
   uint8 internal constant RECEIVE_SHARES_ENABLED_MASK = 0x08;
 
   /// @notice Initializes the ReserveFlags with the given values.
-  /// @param initPaused The initial 'paused' flag status.
-  /// @param initFrozen The initial 'frozen' flag status.
-  /// @param initBorrowable The initial 'borrowable' flag status.
-  /// @param initReceiveSharesEnabled The initial 'receiveSharesEnabled' flag status.
+  /// @param initPaused The initial `paused` flag status.
+  /// @param initFrozen The initial `frozen` flag status.
+  /// @param initBorrowable The initial `borrowable` flag status.
+  /// @param initReceiveSharesEnabled The initial `receiveSharesEnabled` flag status.
   /// @return The initialized ReserveFlags.
   function initFlags(
     bool initPaused,
@@ -39,33 +39,33 @@ library ReserveFlagsMap {
     return ReserveFlags.wrap(flags);
   }
 
-  /// @notice Sets the new status for the 'paused' flag.
+  /// @notice Sets the new status for the `paused` flag.
   /// @param flags The current ReserveFlags.
-  /// @param status The new status for the 'paused' flag.
+  /// @param status The new status for the `paused` flag.
   /// @return The updated ReserveFlags.
   function setPaused(ReserveFlags flags, bool status) internal pure returns (ReserveFlags) {
     return ReserveFlags.wrap(_setFlag(ReserveFlags.unwrap(flags), PAUSED_MASK, status));
   }
 
-  /// @notice Sets the new status for the 'frozen' flag.
+  /// @notice Sets the new status for the `frozen` flag.
   /// @param flags The current ReserveFlags.
-  /// @param status The new status for the 'frozen' flag.
+  /// @param status The new status for the `frozen` flag.
   /// @return The updated ReserveFlags.
   function setFrozen(ReserveFlags flags, bool status) internal pure returns (ReserveFlags) {
     return ReserveFlags.wrap(_setFlag(ReserveFlags.unwrap(flags), FROZEN_MASK, status));
   }
 
-  /// @notice Sets the new status for the 'borrowable' flag.
+  /// @notice Sets the new status for the `borrowable` flag.
   /// @param flags The current ReserveFlags.
-  /// @param status The new status for the 'borrowable' flag.
+  /// @param status The new status for the `borrowable` flag.
   /// @return The updated ReserveFlags.
   function setBorrowable(ReserveFlags flags, bool status) internal pure returns (ReserveFlags) {
     return ReserveFlags.wrap(_setFlag(ReserveFlags.unwrap(flags), BORROWABLE_MASK, status));
   }
 
-  /// @notice Sets the new status for the 'receiveSharesEnabled' flag.
+  /// @notice Sets the new status for the `receiveSharesEnabled` flag.
   /// @param flags The current ReserveFlags.
-  /// @param status The new status for the 'receiveSharesEnabled' flag.
+  /// @param status The new status for the `receiveSharesEnabled` flag.
   /// @return The updated ReserveFlags.
   function setReceiveSharesEnabled(
     ReserveFlags flags,
@@ -75,28 +75,28 @@ library ReserveFlagsMap {
       ReserveFlags.wrap(_setFlag(ReserveFlags.unwrap(flags), RECEIVE_SHARES_ENABLED_MASK, status));
   }
 
-  /// @notice Returns the 'paused' flag status.
+  /// @notice Returns the `paused` flag status.
   /// @param flags The current ReserveFlags.
   /// @return True if the flag is set.
   function paused(ReserveFlags flags) internal pure returns (bool) {
     return (ReserveFlags.unwrap(flags) & PAUSED_MASK) != 0;
   }
 
-  /// @notice Returns the 'frozen' flag status.
+  /// @notice Returns the `frozen` flag status.
   /// @param flags The current ReserveFlags.
   /// @return True if the flag is set.
   function frozen(ReserveFlags flags) internal pure returns (bool) {
     return (ReserveFlags.unwrap(flags) & FROZEN_MASK) != 0;
   }
 
-  /// @notice Returns the 'borrowable' flag status.
+  /// @notice Returns the `borrowable` flag status.
   /// @param flags The current ReserveFlags.
   /// @return True if the flag is set.
   function borrowable(ReserveFlags flags) internal pure returns (bool) {
     return (ReserveFlags.unwrap(flags) & BORROWABLE_MASK) != 0;
   }
 
-  /// @notice Returns the 'receiveSharesEnabled' flag status.
+  /// @notice Returns the `receiveSharesEnabled` flag status.
   /// @param flags The current ReserveFlags.
   /// @return True if the flag is set.
   function receiveSharesEnabled(ReserveFlags flags) internal pure returns (bool) {
