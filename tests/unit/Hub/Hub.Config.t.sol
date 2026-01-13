7cff1c1d0c2406b33b6bcda42a1ee02a428a0b6e
Kogaroshi <25688223+Kogaroshi@users.noreply.github.com>
2026-01-13 09:50:56 +0100

    fix : fix CI tests

--- tests/unit/Hub/Hub.Config.t.sol
+++ tests/unit/Hub/Hub.Config.t.sol
@@ -422,6 +422,7 @@ contract HubConfigTest is HubBase {
     assetId = bound(assetId, 0, hub1.getAssetCount() - 1);
     _assumeValidAssetConfig(newConfig);
     assumeUnusedAddress(newConfig.irStrategy);
+    newConfig.feeReceiver = hub1.getAssetConfig(assetId).feeReceiver;
 
     vm.mockCallRevert(
       newConfig.irStrategy,
@@ -429,7 +430,7 @@ contract HubConfigTest is HubBase {
       'custom revert'
     );
 
-    vm.expectRevert(address(newConfig.irStrategy));
+    vm.expectRevert(abi.encode('custom revert'), newConfig.irStrategy);
     vm.prank(HUB_ADMIN);
     hub1.updateAssetConfig(assetId, newConfig, encodedIrData);
   }
