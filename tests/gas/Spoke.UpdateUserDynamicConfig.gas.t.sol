49d0b1ef5fd9505420c88a63f160913398f6b74b
CheyenneAtapour <CheyenneAtapour@gmail.com>
2026-01-14 17:46:35 +0800

    feat: Finer granularity in dyn config gas snapshot

--- tests/gas/Spoke.UpdateUserDynamicConfig.gas.t.sol
+++ tests/gas/Spoke.UpdateUserDynamicConfig.gas.t.sol
@@ -15,7 +15,7 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
     spoke = spoke1;
   }
 
-  function test_updateUserDynamicConfig_a_1_collateral() public {
+  function test_updateUserDynamicConfig_1_collateral() public {
     _setupCollateralsWithDynamicConfigUpdate(1);
 
     vm.prank(alice);
@@ -23,7 +23,7 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
     vm.snapshotGasLastCall(NAMESPACE, 'A. updateUserDynamicConfig: 1 collateral');
   }
 
-  function test_updateUserDynamicConfig_b_2_collaterals() public {
+  function test_updateUserDynamicConfig_2_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(2);
 
     vm.prank(alice);
@@ -31,52 +31,68 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
     vm.snapshotGasLastCall(NAMESPACE, 'B. updateUserDynamicConfig: 2 collaterals');
   }
 
-  function test_updateUserDynamicConfig_c_5_collaterals() public {
+  function test_updateUserDynamicConfig_3_collaterals() public {
+    _setupCollateralsWithDynamicConfigUpdate(3);
+
+    vm.prank(alice);
+    spoke.updateUserDynamicConfig(alice);
+    vm.snapshotGasLastCall(NAMESPACE, 'C. updateUserDynamicConfig: 3 collaterals');
+  }
+
+  function test_updateUserDynamicConfig_4_collaterals() public {
+    _setupCollateralsWithDynamicConfigUpdate(4);
+
+    vm.prank(alice);
+    spoke.updateUserDynamicConfig(alice);
+    vm.snapshotGasLastCall(NAMESPACE, 'D. updateUserDynamicConfig: 4 collaterals');
+  }
+
+  function test_updateUserDynamicConfig_5_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(5);
 
     vm.prank(alice);
     spoke.updateUserDynamicConfig(alice);
-    vm.snapshotGasLastCall(NAMESPACE, 'C. updateUserDynamicConfig: 5 collaterals');
+    vm.snapshotGasLastCall(NAMESPACE, 'E. updateUserDynamicConfig: 5 collaterals');
   }
 
-  function test_updateUserDynamicConfig_d_10_collaterals() public {
+  function test_updateUserDynamicConfig_10_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(10);
 
     vm.prank(alice);
     spoke.updateUserDynamicConfig(alice);
-    vm.snapshotGasLastCall(NAMESPACE, 'D. updateUserDynamicConfig: 10 collaterals');
+    vm.snapshotGasLastCall(NAMESPACE, 'F. updateUserDynamicConfig: 10 collaterals');
   }
 
-  function test_updateUserDynamicConfig_e_25_collaterals() public {
+  function test_updateUserDynamicConfig_25_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(25);
 
     vm.prank(alice);
     spoke.updateUserDynamicConfig(alice);
-    vm.snapshotGasLastCall(NAMESPACE, 'E. updateUserDynamicConfig: 25 collaterals');
+    vm.snapshotGasLastCall(NAMESPACE, 'G. updateUserDynamicConfig: 25 collaterals');
   }
 
-  function test_updateUserDynamicConfig_f_50_collaterals() public {
+  function test_updateUserDynamicConfig_50_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(50);
 
     vm.prank(alice);
     spoke.updateUserDynamicConfig(alice);
-    vm.snapshotGasLastCall(NAMESPACE, 'F. updateUserDynamicConfig: 50 collaterals');
+    vm.snapshotGasLastCall(NAMESPACE, 'H. updateUserDynamicConfig: 50 collaterals');
   }
 
-  function test_updateUserDynamicConfig_g_100_collaterals() public {
+  function test_updateUserDynamicConfig_100_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(100);
 
     vm.prank(alice);
     spoke.updateUserDynamicConfig(alice);
-    vm.snapshotGasLastCall(NAMESPACE, 'G. updateUserDynamicConfig: 100 collaterals');
+    vm.snapshotGasLastCall(NAMESPACE, 'I. updateUserDynamicConfig: 100 collaterals');
   }
 
-  function test_updateUserDynamicConfig_h_120_collaterals() public {
+  function test_updateUserDynamicConfig_120_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(120);
 
     vm.prank(alice);
     spoke.updateUserDynamicConfig(alice);
-    vm.snapshotGasLastCall(NAMESPACE, 'H. updateUserDynamicConfig: 120 collaterals');
+    vm.snapshotGasLastCall(NAMESPACE, 'J. updateUserDynamicConfig: 120 collaterals');
   }
 
   /// @dev Helper function to set up N collateral reserves with updated dynamic config
