5fbdae70989aa6bdc64c0ac9172a6f511dd68690
CheyenneAtapour <CheyenneAtapour@gmail.com>
2026-01-16 19:53:24 +0800

    fix: update tests and snapshot

--- tests/gas/Spoke.UpdateUserDynamicConfig.gas.t.sol
+++ tests/gas/Spoke.UpdateUserDynamicConfig.gas.t.sol
@@ -97,7 +97,8 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
   function test_liquidateUser_1_collateral() public {
     _setupCollateralsWithDynamicConfigUpdate(1);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -105,13 +106,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'K. liquidateUser: 1 collateral');
   }
 
   function test_liquidateUser_2_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(2);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -119,13 +121,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'L. liquidateUser: 2 collaterals');
   }
 
   function test_liquidateUser_3_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(3);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -133,13 +136,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'M. liquidateUser: 3 collaterals');
   }
 
   function test_liquidateUser_4_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(4);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -147,13 +151,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'N. liquidateUser: 4 collaterals');
   }
 
   function test_liquidateUser_5_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(5);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -161,13 +166,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'O. liquidateUser: 5 collaterals');
   }
 
   function test_liquidateUser_10_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(10);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -175,13 +181,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'P. liquidateUser: 10 collaterals');
   }
 
   function test_liquidateUser_25_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(25);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -189,13 +196,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'Q. liquidateUser: 25 collaterals');
   }
 
   function test_liquidateUser_50_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(50);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -203,13 +211,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'R. liquidateUser: 50 collaterals');
   }
 
   function test_liquidateUser_100_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(100);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -217,13 +226,14 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'S. liquidateUser: 100 collaterals');
   }
 
   function test_liquidateUser_120_collaterals() public {
     _setupCollateralsWithDynamicConfigUpdate(120);
-    uint256 reserveId = spoke.getReserveCount() - 1;
+    uint256 collateralReserveId = spoke.getReserveCount() - 1;
+    uint256 debtReserveId = _daiReserveId(spoke);
 
     // Wait one block so alice is liquidatable
     skip(1);
@@ -231,7 +241,7 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
 
     // Liquidate alice
     vm.prank(bob);
-    spoke.liquidationCall(reserveId, reserveId, alice, 1e18, false);
+    spoke.liquidationCall(collateralReserveId, debtReserveId, alice, 1e18, false);
     vm.snapshotGasLastCall(NAMESPACE, 'T. liquidateUser: 120 collaterals');
   }
 
@@ -243,13 +253,17 @@ contract SpokeUpdateUserDynamicConfig_Gas_Tests is SpokeBase {
     // Collateral factors for these new reserves is 50% to set up liquidations later
     _addNewAssetsAndReserves(collateralCount);
 
-    // Supply all new reserves as collateral for alice, and borrow half of the supply amount
+    // Supply all new reserves as collateral for alice
     uint256 supplyAmount = 100_000e18;
     for (uint256 i = 0; i < collateralCount; ++i) {
       uint256 reserveId = startingReserveId + i;
       Utils.supplyCollateral(spoke, reserveId, alice, supplyAmount, alice);
-      Utils.borrow(spoke, reserveId, alice, supplyAmount / 2, alice);
     }
+
+    // Bob supplies dai to be borrowed by alice
+    Utils.supply(spoke, _daiReserveId(spoke), bob, supplyAmount * collateralCount, bob);
+    // Alice borrows dai at (supplyAmount * collateralCount) / 2
+    Utils.borrow(spoke, _daiReserveId(spoke), alice, (supplyAmount * collateralCount) / 2, alice);
 
     // Update dynamic config for all new reserves to trigger config refresh
     vm.startPrank(SPOKE_ADMIN);
