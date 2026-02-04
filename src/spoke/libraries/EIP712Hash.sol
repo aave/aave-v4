af65216969a2ce0435a49f826b991a017ff0c54a
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2026-02-04 16:59:31 +0530

    opt: simpler hash

--- src/spoke/libraries/EIP712Hash.sol
+++ src/spoke/libraries/EIP712Hash.sol
@@ -35,7 +35,16 @@ library EIP712Hash {
       );
   }
 
-  function hash(ISpoke.PositionManagerUpdate calldata params) internal pure returns (bytes32) {
-    return keccak256(abi.encode(POSITION_MANAGER_UPDATE, params.positionManager, params.approve));
+  function hash(
+    ISpoke.PositionManagerUpdate calldata params
+  ) internal pure returns (bytes32 digest) {
+    assembly ('memory-safe') {
+      let fmp := mload(0x40)
+      mstore(0, POSITION_MANAGER_UPDATE)
+      mstore(0x20, shr(96, shl(96, calldataload(params)))) // params.positionManager
+      mstore(0x40, iszero(iszero(calldataload(add(params, 0x20))))) // params.update
+      digest := keccak256(0, 0x60)
+      mstore(0x40, fmp)
+    }
   }
 }
