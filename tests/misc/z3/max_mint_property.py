2a520afeb2c44ef00af0eaa7076cf4cb84c76f16
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2026-02-10 17:09:01 +0530

    chore: adapt z3

--- tests/misc/z3/max_mint_property.py
+++ tests/misc/z3/max_mint_property.py
@@ -2,50 +2,15 @@
 # maxMint = convertToShares(maxDeposit) = toAddedSharesDown(maxDeposit)
 # When mint is called: assets = previewMint(maxMint) = toAddedAssetsUp(maxMint)
 # _validateAdd checks: allowed >= toAddedAssetsUp(spokeShares) + assets
-from z3 import *
-
-VIRTUAL_SHARES = IntVal(10**6)
-VIRTUAL_ASSETS = IntVal(10**6)
-
-
-def mulDivDown(a, num, den):
-    return (a * num) / den
-
-
-def mulDivUp(a, num, den):
-    return (a * num + den - 1) / den
-
-
-def zeroFloorSub(a, b):
-    return If(a > b, a - b, IntVal(0))
-
-def toAddedAssetsUp(shares, totalAddedAssets, totalAddedShares):
-    """Converts shares to assets, rounding up (previewAddByShares)"""
-    return mulDivUp(
-        shares, totalAddedAssets + VIRTUAL_ASSETS, totalAddedShares + VIRTUAL_SHARES
-    )
+from commons import *
 
 def previewMint(shares, totalAddedAssets, totalAddedShares):
     """Converts shares to assets, rounding up (previewAddByShares)"""
-    return toAddedAssetsUp(shares, totalAddedAssets, totalAddedShares)
+    return previewAddByShares(shares, totalAddedAssets, totalAddedShares)
 
 def convertToShares(assets, totalAddedAssets, totalAddedShares):
     """Converts assets to shares, rounding down (previewAddByAssets)"""
-    return mulDivDown(
-        assets, totalAddedShares + VIRTUAL_SHARES, totalAddedAssets + VIRTUAL_ASSETS
-    )
-
-
-def check(s, propertyDescription):
-    print(f"\n-- {propertyDescription} --")
-    result = s.check()
-    if result == sat:
-        print("Counterexample found:", s.model())
-    elif result == unsat:
-        print("Property holds.")
-    elif result == unknown:
-        print("Timed out or unknown.")
-
+    return previewAddByAssets(assets, totalAddedAssets, totalAddedShares)
 
 totalAddedAssets = Int("totalAddedAssets")
 totalAddedShares = Int("totalAddedShares")
@@ -70,5 +35,4 @@ mintAssets = toAddedAssetsUp(maxMintShares, totalAddedAssets, totalAddedShares)
 s.add(mintAssets > 0)
 hubCheck = toAddedAssetsUp(spokeShares, totalAddedAssets, totalAddedShares) + mintAssets
 
-s.add(Not(allowed >= hubCheck))
-check(s, "mint(maxMint()) satisfies _validateAdd")
+proveValid(s, "mint(maxMint()) satisfies _validateAdd", allowed >= hubCheck)
