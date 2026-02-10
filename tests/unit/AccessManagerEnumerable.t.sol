d8ca3ec95faf04911f35f7c28a32a68ae3825a06
Kogaroshi <25688223+Kogaroshi@users.noreply.github.com>
2026-02-10 14:24:10 +0100

    tests : add extra test for revokeRole

--- tests/unit/AccessManagerEnumerable.t.sol
+++ tests/unit/AccessManagerEnumerable.t.sol
@@ -486,6 +486,24 @@ contract AccessManagerEnumerableTest is Test {
     assertEq(roleMembers[1], user3);
   }
 
+  function test_revokeRole_shouldNotTrack() public {
+    uint64 roleId = 1;
+    address user1 = makeAddr('user1');
+
+    (bool isMember, ) = accessManagerEnumerable.hasRole(roleId, user1);
+    assertFalse(isMember);
+    assertEq(accessManagerEnumerable.getRoleMemberCount(roleId), 0);
+
+    vm.prank(ADMIN);
+    accessManagerEnumerable.revokeRole(roleId, user1);
+
+    assertEq(accessManagerEnumerable.getRoleMemberCount(roleId), 0);
+    assertEq(accessManagerEnumerable.getRoleMembers(roleId, 0, 1).length, 0);
+
+    (isMember, ) = accessManagerEnumerable.hasRole(roleId, user1);
+    assertFalse(isMember);
+  }
+
   function test_renounceRole_shouldNotTrack() public {
     uint64 roleId = 1;
     address user1 = makeAddr('user1');
