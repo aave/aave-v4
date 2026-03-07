c151b67549e00ff1e461f764e6b813f2b3677ed2
YBM <31329384+yan-man@users.noreply.github.com>
2026-03-06 21:21:13 -0600

    fix: revert changes

--- src/hub/AssetInterestRateStrategy.sol
+++ src/hub/AssetInterestRateStrategy.sol
@@ -49,6 +49,10 @@ contract AssetInterestRateStrategy is IAssetInterestRateStrategy {
       InvalidOptimalUsageRatio()
     );
     require(
+      rateData.rateGrowthBeforeOptimal <= rateData.rateGrowthAfterOptimal,
+      GrowthAfterOptimalMustBeGteGrowthBeforeOptimal()
+    );
+    require(
       rateData.baseBorrowRate +
         rateData.rateGrowthBeforeOptimal +
         rateData.rateGrowthAfterOptimal <=
