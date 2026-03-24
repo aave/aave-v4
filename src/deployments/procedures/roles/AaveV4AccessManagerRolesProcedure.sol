4fee07dead18cdae11a02be25fde948d2fea5020
YBM <31329384+yan-man@users.noreply.github.com>
2026-03-24 15:53:51 -0500

    fix: role name

--- src/deployments/procedures/roles/AaveV4AccessManagerRolesProcedure.sol
+++ src/deployments/procedures/roles/AaveV4AccessManagerRolesProcedure.sol
@@ -47,7 +47,7 @@ library AaveV4AccessManagerRolesProcedure {
     RolesValidation.validateNonZeroAddress(accessManager);
     RolesValidation.validateNonZeroAddress(adminToAdd);
     IAccessManager(accessManager).grantRole({
-      roleId: Roles.ACCESS_MANAGER_DEFAULT_ADMIN,
+      roleId: Roles.ACCESS_MANAGER_ADMIN_ROLE,
       account: adminToAdd,
       executionDelay: 0
     });
@@ -57,7 +57,7 @@ library AaveV4AccessManagerRolesProcedure {
     RolesValidation.validateNonZeroAddress(accessManager);
     RolesValidation.validateNonZeroAddress(adminToRemove);
     IAccessManager(accessManager).revokeRole({
-      roleId: Roles.ACCESS_MANAGER_DEFAULT_ADMIN,
+      roleId: Roles.ACCESS_MANAGER_ADMIN_ROLE,
       account: adminToRemove
     });
   }
